71417c397fc4378b21320875dc12721e836807ef
Namhyung Kim <namhyung@gmail.com>
2014-11-21 14:14:26 +0900

    Color log/error message
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- debug.c
+++ debug.c
@@ -14,25 +14,41 @@
 
 #include "utils.h"
 
+#define TERM_COLOR_NORMAL	""
+#define TERM_COLOR_RESET	"\033[m"
+#define TERM_COLOR_BOLD		"\033[1m"
+#define TERM_COLOR_RED		"\033[31m"
+#define TERM_COLOR_GREEN	"\033[32m"
+
 int debug;
 int logfd = STDERR_FILENO;
+
+#define color(C)  write(logfd, C, sizeof(C)-1)
 
 void __pr_log(const char *fmt, ...)
 {
 	va_list ap;
 
+	color(TERM_COLOR_BOLD);
+
 	va_start(ap, fmt);
 	vdprintf(logfd, fmt, ap);
 	va_end(ap);
+
+	color(TERM_COLOR_RESET);
 }
 
 void __pr_err(const char *fmt, ...)
 {
 	va_list ap;
 
+	color(TERM_COLOR_RED);
+
 	va_start(ap, fmt);
 	vdprintf(logfd, fmt, ap);
 	va_end(ap);
+
+	color(TERM_COLOR_RESET);
 
 	exit(1);
 }
@@ -43,11 +59,15 @@ void __pr_err_s(const char *fmt, ...)
 	int saved_errno = errno;
 	char buf[512];
 
+	color(TERM_COLOR_RED);
+
 	va_start(ap, fmt);
 	vdprintf(logfd, fmt, ap);
 	va_end(ap);
 
 	dprintf(logfd, ": %s\n", strerror_r(saved_errno, buf, sizeof(buf)));
+
+	color(TERM_COLOR_RESET);
 
 	exit(1);
 }
