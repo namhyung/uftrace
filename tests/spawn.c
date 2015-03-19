83e4b964a7359d92993c4e773e7605c55e55424a
Namhyung Kim <namhyung@gmail.com>
2015-03-19 15:31:08 +0900

    Add 'spawn' test case
    The spawn test does simply fork + exec.  Note that after exec(),
    symbol will not be shown correctly.  One can use an optional argument
    of the new executable to display the symbols when replay.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/spawn.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define TEST_PROG  "t-abc"
+
+int main(int argc, char *argv[])
+{
+	int pid;
+	char *ch;
+	char buf[4096] = {};
+
+	if (readlink("/proc/self/exe", buf, sizeof(buf)) < 0)
+		strcpy(buf, argv[0]);
+
+	ch = strrchr(buf, '/');
+	if (ch)
+		ch++;
+	else
+		ch = buf;
+
+	strcpy(ch, TEST_PROG);
+
+	pid = fork();
+	if (pid == 0) {
+		execl(buf, TEST_PROG, "1", NULL);
+		exit(2);
+	}
+	waitpid(pid, NULL, 0);
+	return 0;
+}
