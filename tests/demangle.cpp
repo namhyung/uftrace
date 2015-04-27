2274ae80b9d7aa9c90aa2336b350902f1ba6b229
Namhyung Kim <namhyung@gmail.com>
2015-04-27 16:33:24 +0900

    test: Update demangle testcase
    This is to test C++ symbol demangling is work correctly.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- tests/demangle.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(void)
-{
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
-}
---
+++ tests/s-demangle.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class ABC {
+private:
+  int n;
+  int bar(void);
+  int baz(void);
+
+public:
+  ABC(int n);
+  int foo(void);
+};
+
+int __attribute__((noinline)) ABC::foo(void)
+{
+	return bar() + 1;
+}
+
+int __attribute__((noinline)) ABC::bar(void)
+{
+	return baz() - 1;
+}
+
+int __attribute__((noinline)) ABC::baz(void)
+{
+	return n;
+}
+
+ABC::ABC(int c)
+{
+	n = c;
+}
+
+int main(int argc, char *argv[])
+{
+	int n = 0;
+	ABC *abc;
+
+	if (argc > 1)
+		std::cin >> n;
+
+	abc = new ABC(n);
+
+	if (n == abc->foo())
+		return 0;
+	return 1;
+}
