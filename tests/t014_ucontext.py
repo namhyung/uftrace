63c1e25c187fd7286e647befd3db38016b157de6
Namhyung Kim <namhyung@gmail.com>
2015-04-27 16:36:30 +0900

    test: Update testcase for ucontext management functions
    The ucontext functions ({set,get,make,swap}context) can implement a
    user-level thread library.  This test is to check tracing when these
    ucontext function are used.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t014_ucontext.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'ucontext', """
+# DURATION    TID     FUNCTION
+  79.593 us [28383] | __cxa_atexit();
+            [28383] | main() {
+   1.606 us [28383] |   getcontext();
+   1.584 us [28383] |   makecontext();
+            [28383] |   foo() {
+            [28383] |     swapcontext() {
+   5.700 us [28383] |       bar();
+   8.716 us [28383] |     } /* swapcontext */
+   9.489 us [28383] |   } /* foo */
+   0.130 us [28383] |   baz();
+  15.586 us [28383] | } /* main */
+""")
