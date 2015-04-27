043b21985b5807eefb0596311616042006a5c0e3
Namhyung Kim <namhyung@gmail.com>
2015-04-27 09:25:22 +0900

    test: Add testcase for function filtering
    These tests are to test filtering behavior of -F/--filter and/or
    -N/--notrace options on the basic abc testcase.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t004_filter_F.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'abc', """
+# DURATION    TID     FUNCTION
+            [28141] |   a() {
+            [28141] |     b() {
+            [28141] |       c() {
+   0.753 us [28141] |         getpid();
+   1.430 us [28141] |       } /* c */
+   1.915 us [28141] |     } /* b */
+   2.405 us [28141] |   } /* a */
+""")
+
+    def runcmd(self):
+        return '%s -F a %s' % (TestBase.ftrace, 't-abc')
