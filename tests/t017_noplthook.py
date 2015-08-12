5dec4f1bb13f5c05cc41d01b11dd718fe6c19f44
Namhyung Kim <namhyung@gmail.com>
2015-08-12 09:43:07 +0900

    test: Add noplthook test case
    The noplthook test case is a simple test based on t-abc and to check
    --no-plthook option is working as expected.  An example output below:
    
      $ ftrace --no-plthook tests/t-abc
      # DURATION    TID     FUNCTION
                  [28141] | main() {
                  [28141] |   a() {
                  [28141] |     b() {
         1.430 us [28141] |       c();
         1.915 us [28141] |     } /* b */
         2.405 us [28141] |   } /* a */
        72.252 us [28141] | } /* main */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t017_noplthook.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'abc', """
+# DURATION    TID     FUNCTION
+            [28141] | main() {
+            [28141] |   a() {
+            [28141] |     b() {
+   1.430 us [28141] |       c();
+   1.915 us [28141] |     } /* b */
+   2.405 us [28141] |   } /* a */
+  72.252 us [28141] | } /* main */
+""")
+
+    def runcmd(self):
+        return '%s --no-plthook %s' % (TestBase.ftrace, 't-abc')
