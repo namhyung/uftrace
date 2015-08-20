b954a135ce1f66af08e484969bfd7e08bbdf7246
Namhyung Kim <namhyung@gmail.com>
2015-08-20 13:11:04 +0900

    test: Use proper sort() function for filter testcases
    The default sort() function in the TestBase class ignores anything
    before main() for simplicity.  But for filtering test cases, it didn't
    work since there's no main() in the output after a filter applied.
    
    So provide another (simple) sort function which merely collects the
    output as is.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- tests/t018_filter_regex.py
+++ tests/t018_filter_regex.py
@@ -19,3 +19,16 @@ class TestCase(TestBase):
 
     def runcmd(self):
         return '%s -F "^alloc.*$" %s' % (TestBase.ftrace, 't-allocfree')
+
+    def sort(self, output):
+        """ This function post-processes output of the test to be compared .
+            It ignores blank and comment (#) lines and remaining functions.  """
+        result = []
+        for ln in output.split('\n'):
+            # ignore blank lines and comments
+            if ln.strip() == '' or ln.startswith('#'):
+                continue
+            func = ln.split('|', 1)[-1]
+            result.append(func)
+
+        return '\n'.join(result)
--- tests/t020_filter_depth.py
+++ tests/t020_filter_depth.py
@@ -15,3 +15,16 @@ class TestCase(TestBase):
 
     def runcmd(self):
         return '%s -D1 -F "alloc[135]" %s' % (TestBase.ftrace, 't-allocfree')
+
+    def sort(self, output):
+        """ This function post-processes output of the test to be compared .
+            It ignores blank and comment (#) lines and remaining functions.  """
+        result = []
+        for ln in output.split('\n'):
+            # ignore blank lines and comments
+            if ln.strip() == '' or ln.startswith('#'):
+                continue
+            func = ln.split('|', 1)[-1]
+            result.append(func)
+
+        return '\n'.join(result)
