20ea5631e282263eea1d9ef1e2fe24921ca8b3c9
Namhyung Kim <namhyung@gmail.com>
2015-08-21 16:39:04 +0900

    report: Support -k option for kernel function report
    Now ftrace report supports -k option to show kernel functions as well
    in the report.
    
    $ sudo ftrace report -k
      Function                                  Total time   Self time  Nr. called
      ========================================  ==========  ==========  ==========
      __do_page_fault                           587.357 us  587.357 us         118
      SyS_execve                                434.827 us  434.827 us           1
      sys_open                                  288.580 us  288.580 us          36
      sys_read                                  139.946 us  139.946 us          12
      sys_mmap                                  134.567 us  134.567 us          31
      sys_mprotect                              125.201 us  125.201 us          16
      __monstartup                               90.002 us   28.174 us           1
      sys_write                                  85.223 us   85.223 us           3
      main                                       49.684 us    0.747 us           1
      printf                                     46.739 us    8.959 us           1
      ...
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- ftrace.c
+++ ftrace.c
@@ -2703,7 +2703,6 @@ static void sort_by_time(struct rb_root *root, struct trace_entry *te)
 
 static void report_functions(struct ftrace_file_handle *handle)
 {
-	int i;
 	struct sym *sym;
 	struct trace_entry te;
 	struct ftrace_ret_stack *rstack;
@@ -2713,37 +2712,36 @@ static void report_functions(struct ftrace_file_handle *handle)
 	const char f_format[] = "  %-40.40s  %10.10s  %10.10s  %10.10s  \n";
 	const char line[] = "=================================================";
 
-	for (i = 0; i < handle->info.nr_tid; i++) {
-		while ((rstack = get_task_ustack(handle, i)) != NULL) {
-			struct ftrace_task_handle *fth = &tasks[i];
-			struct ftrace_session *sess = find_task_session(fth->tid, rstack->time);
-			struct symtabs *symtabs = &sess->symtabs;
-			struct fstack *fstack = &fth->func_stack[rstack->depth];
+	struct ftrace_task_handle *task;
+	struct ftrace_session *sess;
+	struct fstack *fstack;
 
-			if (rstack->type == FTRACE_ENTRY)
-				goto next;
+	while (read_rstack(handle, &task) >= 0) {
+		rstack = task->rstack;
+		if (rstack->type != FTRACE_EXIT)
+			continue;
 
-			if (sess == NULL)
-				goto next;
+		if (rstack == &task->kstack)
+			sess = first_session;
+		else
+			sess = find_task_session(task->tid, rstack->time);
 
-			sym = find_symtab(symtabs, rstack->addr, proc_maps);
-			if (sym == NULL) {
-				pr_log("cannot find symbol for %lx\n",
-				       rstack->addr);
-				goto next;
-			}
+		if (sess == NULL)
+			continue;
 
-			te.pid = fth->tid;
-			te.sym = sym;
-			te.time_total = fstack->total_time;
-			te.time_self = te.time_total - fstack->child_time;
-			te.nr_called = 1;
+		sym = find_symtab(&sess->symtabs, rstack->addr, NULL);
+		if (sym == NULL)
+			continue;
 
-			insert_entry(&name_tree, &te, false);
+		fstack = &task->func_stack[rstack->depth];
 
-		next:
-			tasks[i].valid = false; /* force re-read */
-		}
+		te.pid = task->tid;
+		te.sym = sym;
+		te.time_total = fstack->total_time;
+		te.time_self = te.time_total - fstack->child_time;
+		te.nr_called = 1;
+
+		insert_entry(&name_tree, &te, false);
 	}
 
 	while (!RB_EMPTY_ROOT(&name_tree)) {
@@ -2886,10 +2884,19 @@ static int command_report(int argc, char *argv[], struct opts *opts)
 {
 	int ret;
 	struct ftrace_file_handle handle;
+	struct ftrace_kernel kern;
 
 	ret = open_data_file(opts, &handle);
 	if (ret < 0)
 		return -1;
+
+	if (opts->kernel && (handle.hdr.feat_mask & KERNEL)) {
+		kern.output_dir = opts->dirname;
+		if (setup_kernel_data(&kern) == 0) {
+			handle.kern = &kern;
+			load_kernel_symbol();
+		}
+	}
 
 	if (opts->tid)
 		setup_task_filter(opts->tid, &handle);
@@ -2898,6 +2905,9 @@ static int command_report(int argc, char *argv[], struct opts *opts)
 		report_threads(&handle);
 	else
 		report_functions(&handle);
+
+	if (handle.kern)
+		finish_kernel_data(handle.kern);
 
 	close_data_file(opts, &handle);
 
