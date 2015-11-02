c80427b9996bddc5148184f752d49b286d93f132
Namhyung Kim <namhyung@gmail.com>
2015-11-02 14:24:31 +0900

    test: Add a test case for ftrace report
    Basic test of ftrace report
    
      $ ftrace record tests/t-sort
      $ ftrace report
        Total time   Self time  Nr. called  Function
        ==========  ==========  ==========  ====================================
          1.152 ms   71.683 us           1  main
          1.080 ms    1.813 us           1  bar
          1.078 ms    1.078 ms           1  usleep
         37.525 us    1.137 us           2  foo
         36.388 us   36.388 us           6  loop
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/s-sort.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+
+void __attribute__((noinline)) foo(void);
+void __attribute__((noinline)) bar(void);
+void __attribute__((noinline)) loop(void);
+
+void loop(void)
+{
+	int i;
+	for (i = 0; i < 1000; i++)
+		asm volatile("" ::: "memory");
+}
+
+void foo(void)
+{
+	loop();
+	loop();
+	loop();
+}
+
+void bar(void)
+{
+	usleep(1000);
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < 10000; i++)
+		asm volatile("" ::: "memory");
+	foo();
+
+	for (i = 0; i < 10000; i++)
+		asm volatile("" ::: "memory");
+	foo();
+
+	for (i = 0; i < 10000; i++)
+		asm volatile("" ::: "memory");
+	bar();
+	return 0;
+}
