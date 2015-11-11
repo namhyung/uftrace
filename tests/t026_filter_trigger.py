c776f8e4a10af2e48f9f15f1b042f60ecadec9d1
Namhyung Kim <namhyung@gmail.com>
2015-11-11 16:26:23 +0900

    test: Add test case for depth trigger with filter
      $ ftrace -F main,alloc3@depth=1 tests/t-allocfree
      # DURATION    TID     FUNCTION
                  [  767] | main() {
                  [  767] |   alloc1() {
                  [  767] |     alloc2() {
         4.223 us [  767] |       alloc3();
         4.848 us [  767] |     } /* alloc2 */
         5.417 us [  767] |   } /* alloc1 */
                  [  767] |   free1() {
                  [  767] |     free2() {
                  [  767] |       free3() {
                  [  767] |         free4() {
                  [  767] |           free5() {
         1.104 us [  767] |             free();
         1.974 us [  767] |           } /* free5 */
         2.289 us [  767] |         } /* free4 */
         2.577 us [  767] |       } /* free3 */
         2.857 us [  767] |     } /* free2 */
         3.188 us [  767] |   } /* free1 */
        66.699 us [  767] | } /* main */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t026_filter_trigger.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'allocfree', """
+# DURATION    TID     FUNCTION
+            [  767] | main() {
+            [  767] |   alloc1() {
+            [  767] |     alloc2() {
+   4.223 us [  767] |       alloc3();
+   4.848 us [  767] |     } /* alloc2 */
+   5.417 us [  767] |   } /* alloc1 */
+            [  767] |   free1() {
+            [  767] |     free2() {
+            [  767] |       free3() {
+            [  767] |         free4() {
+            [  767] |           free5() {
+   1.104 us [  767] |             free();
+   1.974 us [  767] |           } /* free5 */
+   2.289 us [  767] |         } /* free4 */
+   2.577 us [  767] |       } /* free3 */
+   2.857 us [  767] |     } /* free2 */
+   3.188 us [  767] |   } /* free1 */
+  66.699 us [  767] | } /* main */
+""")
+
+    def runcmd(self):
+        return '%s -F "main,alloc3@depth=1" %s' % (TestBase.ftrace, 't-allocfree')
