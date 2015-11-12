0e69efe590e0f4bac7cc32954ce977a4380a6d2e
Namhyung Kim <namhyung@gmail.com>
2015-11-13 00:21:03 +0900

    test: Add test cases for trigger without filters
    First case tests trigger at record time (as well as report time).
    
      $ ftrace -T alloc3@depth=1,free@backtrace
      # DURATION    TID     FUNCTION
       101.601 us [31924] | __monstartup();
         2.047 us [31924] | __cxa_atexit();
                  [31924] | main() {
                  [31924] |   alloc1() {
                  [31924] |     alloc2() {
         3.010 us [31924] |       alloc3();
         4.068 us [31924] |     } /* alloc2 */
         4.611 us [31924] |   } /* alloc1 */
                  [31924] |   free1() {
                  [31924] |     free2() {
                  [31924] |       free3() {
                  [31924] |         free4() {
                  [31924] |           free5() {
        backtrace [31924] | /* [ 0] main */
        backtrace [31924] | /* [ 1] free1 */
        backtrace [31924] | /* [ 2] free2 */
        backtrace [31924] | /* [ 3] free3 */
        backtrace [31924] | /* [ 4] free4 */
        backtrace [31924] | /* [ 5] free5 */
         1.894 us [31924] |             free();
         2.921 us [31924] |           } /* free5 */
         3.504 us [31924] |         } /* free4 */
         4.059 us [31924] |       } /* free3 */
         4.588 us [31924] |     } /* free2 */
         5.177 us [31924] |   } /* free1 */
        11.175 us [31924] | } /* main */
    
    The second case tests trigger at replay time:
    
      $ ftrace record tests/t-allocfree
      $ ftrace replay -T alloc1@depth=2 -T free2@depth=1:backtrace
      # DURATION    TID     FUNCTION
       119.757 us [12561] | __monstartup();
         2.006 us [12561] | __cxa_atexit();
                  [12561] | main() {
                  [12561] |   alloc1() {
                  [12561] |     alloc2() {
         4.499 us [12561] |     } /* alloc2 */
         4.998 us [12561] |   } /* alloc1 */
                  [12561] |   free1() {
        backtrace [12561] | /* [ 0] main */
        backtrace [12561] | /* [ 1] free1 */
                  [12561] |     free2() {
         3.905 us [12561] |     } /* free2 */
         4.392 us [12561] |   } /* free1 */
        10.380 us [12561] | } /* main */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t029_trigger_only.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'allocfree', """
+# DURATION    TID     FUNCTION
+ 101.601 us [31924] | __monstartup();
+   2.047 us [31924] | __cxa_atexit();
+            [31924] | main() {
+            [31924] |   alloc1() {
+            [31924] |     alloc2() {
+   3.010 us [31924] |       alloc3();
+   4.068 us [31924] |     } /* alloc2 */
+   4.611 us [31924] |   } /* alloc1 */
+            [31924] |   free1() {
+            [31924] |     free2() {
+            [31924] |       free3() {
+            [31924] |         free4() {
+            [31924] |           free5() {
+  backtrace [31924] | /* [ 0] main */
+  backtrace [31924] | /* [ 1] free1 */
+  backtrace [31924] | /* [ 2] free2 */
+  backtrace [31924] | /* [ 3] free3 */
+  backtrace [31924] | /* [ 4] free4 */
+  backtrace [31924] | /* [ 5] free5 */
+   1.894 us [31924] |             free();
+   2.921 us [31924] |           } /* free5 */
+   3.504 us [31924] |         } /* free4 */
+   4.059 us [31924] |       } /* free3 */
+   4.588 us [31924] |     } /* free2 */
+   5.177 us [31924] |   } /* free1 */
+  11.175 us [31924] | } /* main */
+""")
+
+    def runcmd(self):
+        return '%s -T "alloc3@depth=1,free@backtrace" %s' % (TestBase.ftrace, 't-allocfree')
