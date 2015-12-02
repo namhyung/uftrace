99128b3b4cf0a16b73785e4242675bac6cab8bb5
Namhyung Kim <namhyung@gmail.com>
2015-12-02 15:26:06 +0900

    test: Add test case for full demangler
    Currently it shows double parenthesis at the end since external
    demangler (in libstdc++) adds it in the symbol name.
    
      $ ftrace --demangle=full -N "ns2.*" tests/t-namespace
      # DURATION    TID     FUNCTION
                  [22757] | main() {
         2.554 us [22757] |   operator new(unsigned long);
         1.040 us [22757] |   ns::ns1::foo::foo(int);
                  [22757] |   ns::ns1::foo::bar() {
                  [22757] |     ns::ns1::foo::bar1() {
                  [22757] |       ns::ns1::foo::bar2() {
                  [22757] |         ns::ns1::foo::bar3() {
         1.360 us [22757] |           malloc();
         1.903 us [22757] |         } /* ns::ns1::foo::bar3() */
         2.276 us [22757] |       } /* ns::ns1::foo::bar2() */
         2.629 us [22757] |     } /* ns::ns1::foo::bar1() */
         1.266 us [22757] |     free();
         4.629 us [22757] |   } /* ns::ns1::foo::bar() */
         1.927 us [22757] |   operator delete(void*);
         0.283 us [22757] |   operator new(unsigned long);
         0.223 us [22757] |   operator delete(void*);
        76.629 us [22757] | } /* main */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t043_full_demangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'namespace', lang="C++", result="""
+# DURATION    TID     FUNCTION
+            [22757] | main() {
+   2.554 us [22757] |   operator new(unsigned long);
+   1.040 us [22757] |   ns::ns1::foo::foo(int);
+            [22757] |   ns::ns1::foo::bar() {
+            [22757] |     ns::ns1::foo::bar1() {
+            [22757] |       ns::ns1::foo::bar2() {
+            [22757] |         ns::ns1::foo::bar3() {
+   1.360 us [22757] |           malloc();
+   1.903 us [22757] |         } /* ns::ns1::foo::bar3() */
+   2.276 us [22757] |       } /* ns::ns1::foo::bar2() */
+   2.629 us [22757] |     } /* ns::ns1::foo::bar1() */
+   1.266 us [22757] |     free();
+   4.629 us [22757] |   } /* ns::ns1::foo::bar() */
+   1.927 us [22757] |   operator delete(void*);
+   0.283 us [22757] |   operator new(unsigned long);
+   0.223 us [22757] |   operator delete(void*);
+  76.629 us [22757] | } /* main */
+""")
+
+    def runcmd(self):
+        return '%s --demangle=full -N "ns2.*" %s' % (TestBase.ftrace, 't-namespace')
