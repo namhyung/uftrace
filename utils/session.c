9ae2d03263fd9e12afed679003a1f359a406eda9
Namhyung Kim <namhyung@gmail.com>
2015-12-18 01:32:49 +0900

    test: Add unit test for task/session search
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/session.c
+++ utils/session.c
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include <assert.h>
 
+#undef  PR_FMT
+#define PR_FMT  "session"
+
 #include "../ftrace.h"
 #include "../libmcount/mcount.h"
 #include "symbol.h"
@@ -14,6 +17,15 @@ static struct rb_root sessions = RB_ROOT;
 
 struct ftrace_session *first_session;
 
+/**
+ * create_session - create a new task session from session message
+ * @msg: ftrace session message read from task file
+ * @dirname: ftrace data directory name
+ * @exename: executable name started this session
+ *
+ * This function allocates a new session started by a task.  The new
+ * session will be added to sessions tree sorted by pid and timestamp.
+ */
 void create_session(struct ftrace_msg_sess *msg, char *dirname, char *exename)
 {
 	struct ftrace_session *s;
@@ -53,6 +65,15 @@ void create_session(struct ftrace_msg_sess *msg, char *dirname, char *exename)
 	rb_insert_color(&s->node, &sessions);
 }
 
+/**
+ * find_session - find a matching session using @pid and @timestamp
+ * @pid: task pid to search
+ * @timestamp: timestamp of task
+ *
+ * This function searches the sessions tree using @pid and @timestamp.
+ * The most recent session that has a smaller than the @timestamp will
+ * be returned.
+ */
 struct ftrace_session *find_session(int pid, uint64_t timestamp)
 {
 	struct ftrace_session *iter;
@@ -102,6 +123,16 @@ static void add_session_ref(struct ftrace_task *task, struct ftrace_session *ses
 	task->sess_last = ref;
 }
 
+/**
+ * find_task_session - find a matching session using @pid and @timestamp
+ * @pid - task pid to search
+ * @timestamp - timestamp of task
+ *
+ * This function searches the sessions tree using @pid and @timestamp.
+ * The most recent session that has a smaller than the @timestamp will
+ * be returned.  If it didn't find a session tries to search sesssion
+ * list of parent or thread-leader.
+ */
 struct ftrace_session *find_task_session(int pid, uint64_t timestamp)
 {
 	struct ftrace_task *t;
@@ -126,6 +157,14 @@ struct ftrace_session *find_task_session(int pid, uint64_t timestamp)
 	return NULL;
 }
 
+/**
+ * create_task - create a new task from task message
+ * @msg: ftrace task message read from task file
+ * @fork: whether it's forked or not (i.e. thread)
+ *
+ * This function creates a new task from @msg and add it to task tree.
+ * The newly created task will have a reference to a session.
+ */
 void create_task(struct ftrace_msg_task *msg, bool fork)
 {
 	struct ftrace_task *t;
@@ -157,6 +196,7 @@ void create_task(struct ftrace_msg_task *msg, bool fork)
 
 	t = xmalloc(sizeof(*t));
 
+	/* msg->pid is a parent pid if forked */
 	t->pid = fork ? msg->tid : msg->pid;
 	t->tid = msg->tid;
 	t->sess_last = NULL;
@@ -170,6 +210,10 @@ void create_task(struct ftrace_msg_task *msg, bool fork)
 	rb_insert_color(&t->node, &task_tree);
 }
 
+/**
+ * find_task - find a matching task by @tid
+ * @tid: task id
+ */
 struct ftrace_task *find_task(int tid)
 {
 	struct ftrace_task *t;
@@ -191,6 +235,15 @@ struct ftrace_task *find_task(int tid)
 	return NULL;
 }
 
+/**
+ * walk_tasks - iterates all tasks and invokes @callback
+ * @callback: function to be called for each task
+ * @arg: argument passed to the @callback
+ *
+ * This function traverses the task tree and invokes @callback with
+ * @arg.  As the @callback returns a non-zero value, it'll stop and
+ * return in the middle.
+ */
 void walk_tasks(walk_tasks_cb_t callback, void *arg)
 {
 	struct rb_node *n = rb_first(&task_tree);
@@ -205,3 +258,256 @@ void walk_tasks(walk_tasks_cb_t callback, void *arg)
 		n = rb_next(n);
 	}
 }
+
+#ifdef UNIT_TEST
+
+TEST_CASE(session_search)
+{
+	int i;
+
+	TEST_EQ(first_session, NULL);
+
+	for (i = 0; i < 1000; i++) {
+		struct ftrace_msg_sess msg = {
+			.task = {
+				.pid = 1,
+				.tid = 1,
+				.time = i * 100,
+			},
+			.namelen = 8,  /* = strlen("unittest") */
+		};
+
+		create_session(&msg, ".", "unittest");
+	}
+
+	TEST_NE(first_session, NULL);
+	TEST_EQ(first_session->pid, 1);
+	TEST_EQ(first_session->start_time, 0);
+
+	for (i = 0; i < 1000; i++) {
+		int t;
+		struct ftrace_session *s;
+
+		t = random() % (1000 * 100);
+		s = find_session(1, t);
+
+		TEST_NE(s, NULL);
+		TEST_EQ(s->pid, 1);
+		TEST_GE(t, s->start_time);
+		TEST_LT(t, s->start_time + 100);
+	}
+
+	return TEST_OK;
+}
+
+TEST_CASE(task_search)
+{
+	struct ftrace_task *task;
+	struct ftrace_session *sess;
+
+	/* 1. create initial task */
+	{
+		struct ftrace_msg_sess smsg = {
+			.task = {
+				.pid = 1,
+				.tid = 1,
+				.time = 100,
+			},
+			.sid = "initial",
+			.namelen = 8,  /* = strlen("unittest") */
+		};
+		struct ftrace_msg_task tmsg = {
+			.pid = 1,
+			.tid = 1,
+			.time = 100,
+		};
+
+		create_session(&smsg, ".", "unittest");
+		create_task(&tmsg, false);
+
+		task = find_task(tmsg.tid);
+
+		TEST_NE(task, NULL);
+		TEST_EQ(task->tid, tmsg.tid);
+		TEST_EQ(task->sess.sess, first_session);
+		TEST_NE(first_session, NULL);
+
+		sess = find_session(tmsg.pid, tmsg.time);
+		TEST_NE(sess, NULL);
+		TEST_EQ(sess->pid, task->pid);
+		TEST_EQ(sess->tid, task->tid);
+	}
+
+	/* 2. fork child task */
+	{
+		struct ftrace_msg_task tmsg = {
+			.pid = 1,  /* ppid */
+			.tid = 2,  /* pid */
+			.time = 200,
+		};
+
+		create_task(&tmsg, true);
+
+		task = find_task(tmsg.tid);
+
+		TEST_NE(task, NULL);
+		TEST_EQ(task->tid, tmsg.tid);
+		TEST_EQ(task->sess.sess, first_session);
+
+		sess = find_task_session(tmsg.pid, tmsg.time);
+		TEST_NE(sess, NULL);
+		TEST_EQ(sess->pid, tmsg.pid);
+		TEST_LE(sess->start_time, tmsg.time);
+	}
+
+	/* 3. create parent thread */
+	{
+		struct ftrace_msg_task tmsg = {
+			.pid = 1,
+			.tid = 3,
+			.time = 300,
+		};
+
+		create_task(&tmsg, false);
+
+		task = find_task(tmsg.tid);
+
+		TEST_NE(task, NULL);
+		TEST_EQ(task->tid, tmsg.tid);
+		TEST_EQ(task->sess.sess, first_session);
+
+		sess = find_task_session(tmsg.pid, tmsg.time);
+		TEST_NE(sess, NULL);
+		TEST_EQ(sess->pid, tmsg.pid);
+		TEST_LE(sess->start_time, tmsg.time);
+	}
+
+	/* 4. create child thread */
+	{
+		struct ftrace_msg_task tmsg = {
+			.pid = 2,
+			.tid = 4,
+			.time = 400,
+		};
+
+		create_task(&tmsg, false);
+
+		task = find_task(tmsg.tid);
+
+		TEST_NE(task, NULL);
+		TEST_EQ(task->tid, tmsg.tid);
+		TEST_EQ(task->sess.sess, first_session);
+
+		sess = find_task_session(tmsg.pid, tmsg.time);
+		TEST_NE(sess, NULL);
+		/* it returned a session from parent so pid is not same */
+		TEST_NE(sess->pid, tmsg.pid);
+		TEST_LE(sess->start_time, tmsg.time);
+	}
+
+	/* 5. exec from child */
+	{
+		struct ftrace_msg_sess smsg = {
+			.task = {
+				.pid = 2,
+				.tid = 4,
+				.time = 500,
+			},
+			.sid = "after_exec",
+			.namelen = 8,  /* = strlen("unittest") */
+		};
+		struct ftrace_msg_task tmsg = {
+			.pid = 2,
+			.tid = 4,
+			.time = 500,
+		};
+
+		create_session(&smsg, ".", "unittest");
+		create_task(&tmsg, false);
+
+		task = find_task(tmsg.tid);
+
+		TEST_NE(task, NULL);
+		TEST_EQ(task->tid, tmsg.tid);
+
+		sess = find_task_session(tmsg.pid, tmsg.time);
+		TEST_NE(sess, NULL);
+		TEST_EQ(sess->pid, task->pid);
+		TEST_EQ(sess->tid, task->tid);
+		TEST_LE(sess->start_time, tmsg.time);
+	}
+
+	/* 6. fork grand-child task */
+	{
+		struct ftrace_msg_task tmsg = {
+			.pid = 4,  /* ppid */
+			.tid = 5,  /* pid */
+			.time = 600,
+		};
+
+		create_task(&tmsg, true);
+
+		task = find_task(tmsg.tid);
+
+		TEST_NE(task, NULL);
+		TEST_EQ(task->tid, tmsg.tid);
+
+		sess = find_task_session(tmsg.pid, tmsg.time);
+		TEST_NE(sess, NULL);
+		TEST_EQ(sess->tid, tmsg.pid);
+		TEST_LE(sess->start_time, tmsg.time);
+	}
+
+	/* 7. create grand-child thread */
+	{
+		struct ftrace_msg_task tmsg = {
+			.pid = 5,
+			.tid = 6,
+			.time = 700,
+		};
+
+		create_task(&tmsg, false);
+
+		task = find_task(tmsg.tid);
+
+		TEST_NE(task, NULL);
+		TEST_EQ(task->tid, tmsg.tid);
+
+		sess = find_task_session(tmsg.pid, tmsg.time);
+		TEST_NE(sess, NULL);
+		/* it returned a session from parent so pid is not same */
+		TEST_NE(sess->pid, tmsg.pid);
+		TEST_LE(sess->start_time, tmsg.time);
+	}
+
+	sess = find_task_session(1, 100);
+	TEST_NE(sess, NULL);
+	TEST_STREQ(sess->sid, "initial");
+
+	sess = find_task_session(2, 200);
+	TEST_NE(sess, NULL);
+	TEST_STREQ(sess->sid, "initial");
+
+	sess = find_task_session(4, 400);
+	TEST_NE(sess, NULL);
+	TEST_STREQ(sess->sid, "initial");
+
+	sess = find_task_session(4, 500);
+	TEST_NE(sess, NULL);
+	TEST_STREQ(sess->sid, "after_exec");
+
+	sess = find_task_session(5, 600);
+	TEST_NE(sess, NULL);
+	TEST_STREQ(sess->sid, "after_exec");
+
+	sess = find_task_session(6, 700);
+	TEST_NE(sess, NULL);
+	TEST_STREQ(sess->sid, "after_exec");
+
+	sess = find_task_session(6, 100);
+	TEST_EQ(sess, NULL);
+
+	return TEST_OK;
+}
+
+#endif /* UNIT_TEST */
