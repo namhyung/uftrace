7f2dc84ff1c667204e638e08d22f9dbddc1fe597
Namhyung Kim <namhyung@gmail.com>
2016-01-05 17:05:09 +0900

    ftrace: Add compiler.h header file
    The compiler.h header is to define various compiler/cpu related macro
    and functions like memory barriers.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ utils/compiler.h
@@ -0,0 +1,14 @@
+#ifndef __FTRACE_COMPILER_H__
+#define __FTRACE_COMPILER_H__
+
+#define compiler_barrier()  asm volatile("" :::"memory")
+
+#if defined(__x86_64__)
+# define cpu_relax()	asm volatile("rep; nop" ::: "memory")
+#endif
+
+#if defined(__arm__)
+# define cpu_relax()	compiler_barrier()
+#endif
+
+#endif /* __FTRACE_COMPILER_H__ */
