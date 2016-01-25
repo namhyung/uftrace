f579c78695c9bcc0485d94d010924dda3362fa61
Namhyung Kim <namhyung@gmail.com>
2016-01-25 15:29:43 +0900

    test: Update existing signal test case
    The existing signal test is too simple, add some more code to before and
    after the signal handler to see it working properly.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- tests/s-signal.c
+++ tests/s-signal.c
@@ -1,9 +1,27 @@
 #include <signal.h>
 #include <stdlib.h>
 
+__attribute__((noinline)) void foo(void);
+__attribute__((noinline)) void bar(int n);
+
+volatile int dummy;
+
+void foo(void)
+{
+	if (dummy == 0)
+		dummy = 1;
+	else
+		dummy = 0;
+}
+
+void bar(int n)
+{
+	dummy = n;
+}
+
 void sighandler(int sig)
 {
-	/* do nothing */
+	bar(sig);
 }
 
 int main(int argc, char *argv[])
@@ -13,8 +31,10 @@ int main(int argc, char *argv[])
 	if (argc > 1)
 		sig = atoi(argv[1]);
 
+	foo();
 	signal(sig, sighandler);
 	raise(sig);
+	foo();
 
 	return 0;
 }
