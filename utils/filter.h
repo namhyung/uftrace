e92ff4a6aeddb53371734f78f27e5607b4047a5d
Namhyung Kim <namhyung@gmail.com>
2016-01-26 16:14:32 +0900

    filter: Return matched filter when match
    Instead of returning int value, return matched filter itself to make it
    easy to process.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/filter.h
+++ utils/filter.h
@@ -46,7 +46,7 @@ void ftrace_setup_filter(char *filter_str, struct symtabs *symtabs,
 			 enum filter_mode *mode);
 void ftrace_setup_trigger(char *trigger_str, struct symtabs *symtabs,
 			  char *module, struct rb_root *root);
-int ftrace_match_filter(struct rb_root *root, unsigned long ip,
+struct ftrace_filter *ftrace_match_filter(struct rb_root *root, unsigned long ip,
 			struct ftrace_trigger *tr);
 void ftrace_cleanup_filter(struct rb_root *root);
 void ftrace_print_filter(struct rb_root *root);
