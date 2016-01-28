ff2081a6a129a5edd54258c0a593265491719930
Namhyung Kim <namhyung@gmail.com>
2016-01-28 22:26:38 +0900

    symbol: Print correct dynamic symbol names
    In print_symtabs() it used dsymtab->sym and dsymtab->sym_names
    intermixed.  They're managed by different indices so mixing them is not
    correct.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/symbol.c
+++ utils/symbol.c
@@ -775,9 +775,9 @@ void print_symtabs(struct symtabs *symtabs)
 	pr_out("Dynamic symbols\n");
 	printf("===============\n");
 	for (i = 0; i < dtab->nr_sym; i++) {
-		name = symbol_getname(&dtab->sym[i], dtab->sym[i].addr);
+		name = symbol_getname(dtab->sym_names[i], dtab->sym_names[i]->addr);
 		printf("[%2zd] %#lx: %s (size: %u)\n", i, dtab->sym_names[i]->addr,
 		       name, dtab->sym_names[i]->size);
-		symbol_putname(&dtab->sym[i], name);
+		symbol_putname(dtab->sym_names[i], name);
 	}
 }
