95b13602f37775092e4feb98ca583032e37067d5
Namhyung Kim <namhyung@gmail.com>
2016-02-11 22:44:27 +0900

    test: Add a test case for --no-pltbind
    The --no-pltbind option can show concurrent blocking functions like
    pthread_barrier_wait().  But it hurts performance though.
    
      $ ftrace --no-pltbind --column-view
      # DURATION    TID     FUNCTION
         4.389 us [22769] | __monstartup();
         4.055 us [22769] | __cxa_atexit();
                  [22769] | main() {
         3.394 us [22769] |   pthread_barrier_init();
        50.840 us [22769] |   pthread_create();
        97.218 us [22769] |   pthread_create();
                  [22769] |   racy_count() {
                  [22769] |     pthread_barrier_wait() {
                  [22771] |                 thread_fn() {
                  [22771] |                   racy_count() {
                  [22771] |                     pthread_barrier_wait() {
                  [22772] |                                 thread_fn() {
                  [22772] |                                   racy_count() {
        18.540 us [22772] |                                     pthread_barrier_wait();
       524.562 us [22769] |     } /* pthread_barrier_wait */
       436.249 us [22771] |                     } /* pthread_barrier_wait */
       235.130 us [22772] |                                   } /* racy_count */
       243.543 us [22772] |                                 } /* thread_fn */
       737.159 us [22769] |   } /* racy_count */
       620.232 us [22771] |                   } /* racy_count */
                  [22769] |   pthread_join() {
                  [22771] |                 } /* thread_fn */
        79.555 us [22769] |   } /* pthread_join */
       131.595 us [22769] |   pthread_join();
         1.135 ms [22769] | } /* main */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t050_no_pltbind.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import re
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'racycount', ldflags='-pthread', result="""
+# DURATION    TID     FUNCTION
+            [22829] | __monstartup() {
+  17.753 us [22829] | } /* __monstartup */
+            [22829] | __cxa_atexit() {
+   8.038 us [22829] | } /* __cxa_atexit */
+            [22829] | main() {
+            [22829] |   pthread_barrier_init() {
+   6.095 us [22829] |   } /* pthread_barrier_init */
+            [22829] |   pthread_create() {
+  81.734 us [22829] |   } /* pthread_create */
+            [22829] |   pthread_create() {
+  85.171 us [22829] |   } /* pthread_create */
+            [22829] |   racy_count() {
+            [22829] |     pthread_barrier_wait() {
+            [22832] |                 thread_fn() {
+            [22832] |                   racy_count() {
+            [22831] |                                 thread_fn() {
+            [22832] |                     pthread_barrier_wait() {
+            [22831] |                                   racy_count() {
+            [22831] |                                     pthread_barrier_wait() {
+  21.614 us [22831] |                                     } /* pthread_barrier_wait */
+ 246.706 us [22829] |     } /* pthread_barrier_wait */
+  78.105 us [22832] |                     } /* pthread_barrier_wait */
+ 300.416 us [22831] |                                   } /* racy_count */
+ 314.149 us [22831] |                                 } /* thread_fn */
+ 567.337 us [22829] |   } /* racy_count */
+            [22829] |   pthread_join() {
+ 370.700 us [22832] |                   } /* racy_count */
+ 383.658 us [22832] |                 } /* thread_fn */
+ 334.627 us [22829] |   } /* pthread_join */
+            [22829] |   pthread_join() {
+   5.735 us [22829] |   } /* pthread_join */
+   1.122 ms [22829] | } /* main */
+""")
+
+    def runcmd(self):
+        return '%s --no-pltbind --column-view --no-merge %s' % (TestBase.ftrace, 't-' + self.name)
