4140833aaacfb19da3a1ac2109bcad101c127380
Namhyung Kim <namhyung@gmail.com>
2016-02-16 11:27:36 +0900

    test: Add more tests which can affect mcount
    Returning large size value (struct type) or calling nested function can
    affect setting up frame pointer before mcount.  Add test cases for those
    so that we can verify the libmcount is working properly.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t051_return.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import re
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'return', result="""
+# DURATION    TID     FUNCTION
+            [12703] | main() {
+            [12703] |   return_large() {
+   1.440 us [12703] |     memset();
+   2.533 us [12703] |   } /* return_large */
+   0.153 us [12703] |   return_small();
+   0.157 us [12703] |   return_long_double();
+   4.097 us [12703] | } /* main */
+""")
---
+++ tests/t052_nested_func.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import re
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'nested', result="""
+# DURATION    TID     FUNCTION
+            [13348] | main() {
+            [13348] |   foo() {
+   0.170 us [13348] |     foo_internal.2406();
+   0.650 us [13348] |   } /* foo */
+            [13348] |   bar() {
+            [13348] |     qsort() {
+   0.120 us [13348] |       compar.2414();
+   0.093 us [13348] |       compar.2414();
+   0.092 us [13348] |       compar.2414();
+   2.479 us [13348] |     } /* qsort */
+   3.462 us [13348] |   } /* bar */
+   3.623 us [13348] | } /* main */
+""")
+
+    def sort(self, output, ignore_children=False):
+        """ This function post-processes output of the test to be compared .
+            It ignores blank and comment (#) lines and remaining functions.  """
+        before_main = True
+        funcs = []
+        for ln in output.split('\n'):
+            if ln.find(' | main()') > 0:
+                before_main = False
+            if before_main:
+                continue
+            # ignore result of remaining functions which follows a blank line
+            if ln.strip() == '':
+                break;
+
+            try:
+                func = ln.split('|', 1)[-1]
+                # ignore function suffix after '.'
+                funcs.append(func.split('.',1)[0])
+            except:
+                pass
+
+        result = '\n'.join(funcs)
+        return result
