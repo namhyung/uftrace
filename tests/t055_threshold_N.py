9e9812ce58bd477440952785004032a47c5134db
Namhyung Kim <namhyung@gmail.com>
2016-02-19 22:58:07 +0900

    test: Add new test cases for time threshold
    Verify time filter with existing (filter) options.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t055_threshold_N.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import re
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'sleep', result="""
+# DURATION    TID     FUNCTION
+            [18224] | main() {
+   2.083 ms [18224] |   foo();
+   2.085 ms [18224] | } /* main */
+""")
+
+    def runcmd(self):
+        return '%s -r 1ms -N bar %s' % (TestBase.ftrace, 't-' + self.name)
