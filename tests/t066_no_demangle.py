099afd579f175d398456b3407bfe5ce1340b28ff
Namhyung Kim <namhyung@gmail.com>
2016-05-11 16:10:09 +0900

    test: Add a test case for no demangling
    There was a bug when filtering with non-demangled symbols.  Add a test
    case to check it.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t066_no_demangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'namespace', lang="C++", result="""
+# DURATION    TID     FUNCTION
+            [17005] | _ZN2ns3ns13foo3barEv() {
+            [17005] |   _ZN2ns3ns13foo4bar1Ev() {
+            [17005] |     _ZN2ns3ns13foo4bar2Ev() {
+            [17005] |       _ZN2ns3ns13foo4bar3Ev() {
+   1.350 us [17005] |         malloc();
+   3.245 us [17005] |       } /* _ZN2ns3ns13foo4bar3Ev */
+   3.705 us [17005] |     } /* _ZN2ns3ns13foo4bar2Ev */
+   4.128 us [17005] |   } /* _ZN2ns3ns13foo4bar1Ev */
+   1.463 us [17005] |   free();
+   6.702 us [17005] | } /* _ZN2ns3ns13foo3barEv */
+""")
+
+    def runcmd(self):
+        return '%s --demangle=no -F "_ZN2ns3ns13foo3barEv" %s' % \
+            (TestBase.ftrace, 't-namespace')
