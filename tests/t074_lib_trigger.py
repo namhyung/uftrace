215c6c2407e0853f57b2ee195cd98c3b0bc0c4f9
Namhyung Kim <namhyung@gmail.com>
2016-07-26 15:01:22 +0900

    test: Add some test cases for library tracing
    The libabc_test_lib.so has lib_a, lib_b and lib_c functions.  Write test
    cases to check tracing these functions work properly.
    
      $ uftrace t-lib
      ftrace: /home/namhyung/project/uftrace/cmd-record.c:1288:check_binary
       ERROR: Can't find 'mcount' symbol in the 't-lib'.
              It seems not to be compiled with -pg or -finstrument-functions flag
              which generates traceable code.  Please check your binary file.
    
      $ uftrace --force --no-libcall t-lib
      # DURATION    TID     FUNCTION
                  [21569] | lib_a() {
                  [21569] |   lib_b() {
         1.509 us [21569] |     lib_c();
         3.543 us [21569] |   } /* lib_b */
         5.434 us [21569] | } /* lib_a */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t074_lib_trigger.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'lib', """
+# DURATION    TID     FUNCTION
+            [17457] | lib_a() {
+   6.911 us [17457] |   lib_b();
+   8.279 us [17457] | } /* lib_a */
+""")
+
+    def build(self, cflags='', ldflags=''):
+        import os
+        import subprocess as sp
+
+        if self.lang not in TestBase.supported_lang:
+#            print("%s: unsupported language: %s" % (self.name, self.lang))
+            return TestBase.TEST_UNSUPP_LANG
+
+        lang = TestBase.supported_lang[self.lang]
+        prog = 't-' + self.name
+
+        build_cflags  = ' '.join([self.cflags, cflags, \
+                                  os.getenv(lang['flags'], '')])
+        build_ldflags = ' '.join([self.ldflags, ldflags, \
+                                  os.getenv('LDFLAGS', '')])
+
+        lib_cflags = build_cflags + ' -shared -fPIC'
+
+        # build libabc_test_lib.so library
+        build_cmd = '%s -o libabc_test_lib.so %s s-lib.c %s' % \
+                    (lang['cc'], lib_cflags, build_ldflags)
+
+#        print("build command for library: %s" % build_cmd)
+        if sp.call(build_cmd.split(), stdout=sp.PIPE) < 0:
+            return TestBase.TEST_BUILD_FAIL
+
+        exe_ldflags = build_ldflags + ' -Wl,-rpath,"$ORIGIN" -L. -labc_test_lib'
+
+        build_cmd = '%s -o %s s-libmain.c %s' % \
+                    (lang['cc'], prog, exe_ldflags)
+
+#        print("build command for executable: %s" % build_cmd)
+        if sp.call(build_cmd.split(), stdout=sp.PIPE) < 0:
+            return TestBase.TEST_BUILD_FAIL
+        return 0
+
+    def sort(self, output, ignore_children=False):
+        """ This function post-processes output of the test to be compared .
+            It ignores blank and comment (#) lines and remaining functions.  """
+        result = []
+        for ln in output.split('\n'):
+            # ignore blank lines and header
+            if ln.strip() == '' or ln.startswith('#'):
+                continue
+            func = ln.split('|', 1)[-1]
+            result.append(func)
+        return '\n'.join(result)
+
+    def runcmd(self):
+        return '%s --force --no-libcall -T lib_b@libabc_test,depth=1 %s' % (TestBase.ftrace, 't-' + self.name)
---
+++ tests/t077_lib_replay_T.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+import subprocess as sp
+
+TDIR='xxx'
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'lib', """
+# DURATION    TID     FUNCTION
+            [17460] | lib_a() {
+   6.911 us [17460] |   lib_b();
+   8.279 us [17460] | } /* lib_a */
+""")
+
+    def build(self, cflags='', ldflags=''):
+        import os
+        import subprocess as sp
+
+        if self.lang not in TestBase.supported_lang:
+#            print("%s: unsupported language: %s" % (self.name, self.lang))
+            return TestBase.TEST_UNSUPP_LANG
+
+        lang = TestBase.supported_lang[self.lang]
+        prog = 't-' + self.name
+
+        build_cflags  = ' '.join([self.cflags, cflags, \
+                                  os.getenv(lang['flags'], '')])
+        build_ldflags = ' '.join([self.ldflags, ldflags, \
+                                  os.getenv('LDFLAGS', '')])
+
+        lib_cflags = build_cflags + ' -shared -fPIC'
+
+        # build libabc_test_lib.so library
+        build_cmd = '%s -o libabc_test_lib.so %s s-lib.c %s' % \
+                    (lang['cc'], lib_cflags, build_ldflags)
+
+#        print("build command for library: %s" % build_cmd)
+        if sp.call(build_cmd.split(), stdout=sp.PIPE) < 0:
+            return TestBase.TEST_BUILD_FAIL
+
+        exe_ldflags = build_ldflags + ' -Wl,-rpath,"$ORIGIN" -L. -labc_test_lib'
+
+        build_cmd = '%s -o %s s-libmain.c %s' % \
+                    (lang['cc'], prog, exe_ldflags)
+
+#        print("build command for executable: %s" % build_cmd)
+        if sp.call(build_cmd.split(), stdout=sp.PIPE) < 0:
+            return TestBase.TEST_BUILD_FAIL
+        return 0
+
+    def sort(self, output, ignore_children=False):
+        """ This function post-processes output of the test to be compared .
+            It ignores blank and comment (#) lines and remaining functions.  """
+        result = []
+        for ln in output.split('\n'):
+            # ignore blank lines and header
+            if ln.strip() == '' or ln.startswith('#'):
+                continue
+            func = ln.split('|', 1)[-1]
+            result.append(func)
+        return '\n'.join(result)
+
+    def pre(self):
+        record_cmd = '%s record --force --no-libcall -d %s %s' % (TestBase.ftrace, TDIR, 't-' + self.name)
+        sp.call(record_cmd.split())
+        return TestBase.TEST_SUCCESS
+
+    def runcmd(self):
+        return '%s replay -d %s -T lib_a@libabc_test,depth=2' % (TestBase.ftrace, TDIR)
+
+    def post(self, ret):
+        sp.call(['rm', '-rf', TDIR])
+        return ret
