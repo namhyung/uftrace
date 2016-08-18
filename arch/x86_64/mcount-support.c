7a4bc2733da5701d9532f14a8415598496e90cd5
Namhyung Kim <namhyung@gmail.com>
2016-08-18 20:27:37 +0900

    arch/x86: Support register and stack arguments
    When user gives specific register name or stack offset, use the
    information directly.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- arch/x86_64/mcount-support.c
+++ arch/x86_64/mcount-support.c
@@ -9,10 +9,22 @@ int mcount_get_register_arg(struct mcount_arg_context *ctx,
 			    struct ftrace_arg_spec *spec)
 {
 	struct mcount_regs *regs = ctx->regs;
-	int reg_idx = spec->idx;
+	int reg_idx;
 
-	if (spec->type == ARG_TYPE_FLOAT)
-		reg_idx += X86_REG_FLOAT_BASE;
+	switch (spec->type) {
+	case ARG_TYPE_REG:
+		reg_idx = spec->reg_idx;
+		break;
+	case ARG_TYPE_INDEX:
+		reg_idx = spec->idx; /* for integer arguments */
+		break;
+	case ARG_TYPE_FLOAT:
+		reg_idx = spec->idx + X86_REG_FLOAT_BASE;
+		break;
+	case ARG_TYPE_STACK:
+	default:
+		return -1;
+	}
 
 	switch (reg_idx) {
 	case X86_REG_RDI:
@@ -67,10 +79,24 @@ int mcount_get_register_arg(struct mcount_arg_context *ctx,
 void mcount_get_stack_arg(struct mcount_arg_context *ctx,
 			  struct ftrace_arg_spec *spec)
 {
-	int offset = spec->idx - ARCH_MAX_REG_ARGS;
+	int offset;
 
-	if (spec->type == ARG_TYPE_FLOAT)
+	switch (spec->type) {
+	case ARG_TYPE_STACK:
+		offset = spec->stack_ofs;
+		break;
+	case ARG_TYPE_INDEX:
+		offset = spec->idx - ARCH_MAX_REG_ARGS;
+		break;
+	case ARG_TYPE_FLOAT:
 		offset = (spec->idx - ARCH_MAX_FLOAT_REGS) * 2 - 1;
+		break;
+	case ARG_TYPE_REG:
+	default:
+		/* should not reach here */
+		pr_err_ns("invalid stack access for arguments\n");
+		break;
+	}
 
 	if (offset < 1 || offset > 100)
 		pr_dbg("invalid stack offset: %d\n", offset);
@@ -88,7 +114,7 @@ void mcount_arch_get_arg(struct mcount_arg_context *ctx,
 void mcount_arch_get_retval(struct mcount_arg_context *ctx,
 			    struct ftrace_arg_spec *spec)
 {
-	/* type of return value is always INDEX, so check format instead */
+	/* type of return value cannot be FLOAT, so check format instead */
 	if (spec->fmt != ARG_FMT_FLOAT)
 		memcpy(ctx->val.v, ctx->retval, spec->size);
 	else
