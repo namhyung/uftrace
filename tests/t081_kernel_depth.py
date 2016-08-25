cdbd4107c1f52c4b36121fea2b87567d148e890f
Namhyung Kim <namhyung@gmail.com>
2016-08-25 16:16:26 +0900

    test: Add a test case for kernel depth filter
      $ sudo uftrace -K --kernel-skip-out --kernel-depth 2 \
      > -N exit_to_usermode_loop@kernel tests/t-openclose
      # DURATION    TID     FUNCTION
         1.602 us [27731] | __monstartup();
         0.763 us [27731] | __cxa_atexit();
                  [27731] | main() {
                  [27731] |   open() {
                  [27731] |     sys_open() {
        13.661 us [27731] |       do_sys_open();
        15.324 us [27731] |     } /* sys_open */
        17.630 us [27731] |   } /* open */
                  [27731] |   close() {
                  [27731] |     sys_close() {
         1.003 us [27731] |       __close_fd();
         2.380 us [27731] |     } /* sys_close */
         9.008 us [27731] |   } /* close */
        28.918 us [27731] | } /* main */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t081_kernel_depth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+import os
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'openclose', """
+# DURATION    TID     FUNCTION
+   1.540 us [27711] | __monstartup();
+   1.089 us [27711] | __cxa_atexit();
+            [27711] | main() {
+            [27711] |   open() {
+            [27711] |     sys_open() {
+  12.732 us [27711] |       do_sys_open();
+  14.039 us [27711] |     } /* sys_open */
+  17.193 us [27711] |   } /* open */
+            [27711] |   close() {
+            [27711] |     sys_close() {
+   0.591 us [27711] |       __close_fd();
+   1.429 us [27711] |     } /* sys_close */
+   8.028 us [27711] |   } /* close */
+  26.938 us [27711] | } /* main */
+""")
+
+    def pre(self):
+        if os.geteuid() != 0:
+            return TestBase.TEST_SKIP
+        return TestBase.TEST_SUCCESS
+
+    def runcmd(self):
+        return '%s -K --kernel-depth=2 --kernel-skip-out -N %s@kernel %s' % \
+            (TestBase.ftrace, 'exit_to_usermode_loop', 't-' + self.name)
