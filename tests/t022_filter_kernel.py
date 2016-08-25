e0c0d61c59ca4ddd53f15264ca72a7781bd8a96c
Namhyung Kim <namhyung@gmail.com>
2016-08-25 15:26:36 +0900

    test: Update filter-kernel test
    The 'sys_get*@kernel' filter is improper for testing since it matches to
    many kernel functions but in-kernel graph-filter size is fixed to 32.
    The end result was that the sys_getegid() was not filtered due to the
    size limit.
    
    So update filter to only show sys_get-euid/egid functions.  This also
    verifies recent change of the syscall name fallback mechanism.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- tests/t022_filter_kernel.py
+++ tests/t022_filter_kernel.py
@@ -9,20 +9,14 @@ class TestCase(TestBase):
 # DURATION    TID     FUNCTION
             [20769] | main() {
    0.925 us [20769] |   getpid();
-            [20769] |   getppid() {
-   0.346 us [20769] |     sys_getppid();
-   2.089 us [20769] |   } /* getppid */
+   2.089 us [20769] |   getppid();
    1.334 us [20769] |   getpgid();
    0.881 us [20769] |   getsid();
-            [20769] |   getuid() {
-   0.092 us [20769] |     sys_getuid();
-   1.234 us [20769] |   } /* getuid */
+   1.234 us [20769] |   getuid();
             [20769] |   geteuid() {
    0.056 us [20769] |     sys_geteuid();
    1.178 us [20769] |   } /* geteuid */
-            [20769] |   getgid() {
-   0.057 us [20769] |     sys_getgid();
-   0.994 us [20769] |   } /* getgid */
+   0.994 us [20769] |   getgid();
             [20769] |   getegid() {
    0.054 us [20769] |     sys_getegid();
    0.912 us [20769] |   } /* getegid */
@@ -35,4 +29,4 @@ class TestCase(TestBase):
         return TestBase.TEST_SUCCESS
 
     def runcmd(self):
-        return '%s -k -F "sys_get*@kernel" %s' % (TestBase.ftrace, 't-getids')
+        return '%s -k -F "sys_gete*@kernel" %s' % (TestBase.ftrace, 't-getids')
