fd0819abec8175deb14728dc2c1674a6c251dea3
Namhyung Kim <namhyung@gmail.com>
2016-09-12 15:43:30 +0900

    test: Add test cases for graph
    The graph command builds function graph from the recorded data.  It
    failed to run when the data has invalid/unmatched function entry/exit
    pairs.  However it's common if the workload calls special functions like
    exit(), fork() or exec().  So make the graph run on those data too.
    
    Add test cases for such cases.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/s-exit.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+static void foo(int status)
+{
+  exit(status);
+}
+
+int main(int argc, char *argv[])
+{
+  foo(argc - 1);
+  return 0;
+}
---
+++ tests/t089_graph_exit.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+import subprocess as sp
+
+TDIR='xxx'
+FUNC='main'
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'exit', result="""
+#
+# function graph for 'main' (session: 924133e5b1cd2228)
+#
+
+backtrace
+================================
+ backtrace #0: hit 1, time           
+   [0] main (0x400644)
+
+calling functions
+================================
+            : (1) main
+            : (1) foo
+            : (1) exit
+""")
+
+    def pre(self):
+        record_cmd = '%s record -d %s %s' % (TestBase.ftrace, TDIR, 't-' + self.name)
+        sp.call(record_cmd.split())
+        return TestBase.TEST_SUCCESS
+
+    def runcmd(self):
+        return '%s graph -d %s %s' % (TestBase.ftrace, TDIR, FUNC)
+
+    def post(self, ret):
+        sp.call(['rm', '-rf', TDIR])
+        return ret
+
+    def sort(self, output):
+        """ This function post-processes output of the test to be compared.
+            It ignores blank and comment (#) lines and header lines.  """
+        result = []
+        mode = 0
+        for ln in output.split('\n'):
+            if ln.strip() == '' or ln.startswith('#') or ln.startswith('='):
+                continue
+            if ln.startswith('backtrace'):
+                mode = 1
+                continue
+            if ln.startswith('calling'):
+                mode = 2
+                continue
+            if mode == 1:
+                if ln.startswith(' backtrace #'):
+                    result.append(ln.split(',')[0])  # remove time part
+                if ln.startswith('   ['):
+                    result.append(ln.split('(')[0])  # remove '(addr)' part
+            if mode == 2:
+                result.append(ln.split(':')[1])      # remove time part
+
+        return '\n'.join(result)
