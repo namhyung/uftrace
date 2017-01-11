2300a9c77669083e9aec97d7dbe995650c698312
Namhyung Kim <namhyung@gmail.com>
2017-01-11 09:14:55 +0900

    test: Simplify *fork + exec test cases
    As it runs the test case in the tests directory, it should find the
    target program in the same (current) directry.  So just use the name of
    binary as relative path and get rid of the readlink stuff.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- tests/s-forkexec.c
+++ tests/s-forkexec.c
@@ -12,23 +12,10 @@
 int main(int argc, char *argv[])
 {
 	int pid;
-	char *ch;
-	char buf[4096] = {};
-
-	if (readlink("/proc/self/exe", buf, sizeof(buf)) < 0)
-		strcpy(buf, argv[0]);
-
-	ch = strrchr(buf, '/');
-	if (ch)
-		ch++;
-	else
-		ch = buf;
-
-	strcpy(ch, TEST_PROG);
 
 	pid = fork();
 	if (pid == 0) {
-		execl(buf, TEST_PROG, "1", NULL);
+		execl(TEST_PROG, TEST_PROG, NULL);
 		exit(2);
 	}
 	waitpid(pid, NULL, 0);
