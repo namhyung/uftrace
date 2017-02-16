c268b35f79323a38347f0569ff107723de50253a
Namhyung Kim <namhyung@gmail.com>
2017-02-16 20:20:44 +0900

    test: Update kernel test cases
    With recent change to use poll for kernel recording, it generates many
    interrupts to wake tasks.  Add -N filter to skip the function.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- tests/t103_dump_kernel.py
+++ tests/t103_dump_kernel.py
@@ -54,7 +54,8 @@ class TestCase(TestBase):
         if os.path.exists('/.dockerenv'):
             return TestBase.TEST_SKIP
 
-        record_cmd = '%s record -k -d %s %s' % (TestBase.ftrace, TDIR, 't-' + self.name)
+        record_cmd = '%s record -k -N %s@kernel -d %s %s' % \
+                     (TestBase.ftrace, 'smp_irq_work_interrupt', TDIR, 't-' + self.name)
         sp.call(record_cmd.split())
         return TestBase.TEST_SUCCESS
 
