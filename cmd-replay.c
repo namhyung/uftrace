9c9e9c463cc15e09c48b2664e098ea245138e273
Namhyung Kim <namhyung@gmail.com>
2017-03-16 15:09:05 +0900

    replay: Escape a newline at the end of string
    String arguments often end with a newline character.  In replay output,
    those strings break alignment so it'd be better to escape them out.
    
    For example, consider following case:
    
      $ uftrace -A printf@arg1/s hello
      hello world
      # DURATION    TID     FUNCTION
         2.177 us [ 3925] | __monstartup();
         0.720 us [ 3925] | __cxa_atexit();
                  [ 3925] | main() {
         8.420 us [ 3925] |   printf("hello %s
       ");
         9.622 us [ 3925] | } /* main */
    
    The newline character after '%s' was shown as is and broke the output.
    Now it's displayed as escaped like in a source code.
    
      $ uftrace -A printf@arg1/s hello
      hello world
      # DURATION    TID     FUNCTION
         2.283 us [ 3937] | __monstartup();
         0.719 us [ 3937] | __cxa_atexit();
                  [ 3937] | main() {
         8.812 us [ 3937] |   printf("hello %s\n");
         9.913 us [ 3937] | } /* main */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- cmd-replay.c
+++ cmd-replay.c
@@ -469,11 +469,24 @@ void get_argspec_string(struct ftrace_task_handle *task,
 
 		if (spec->fmt == ARG_FMT_STR) {
 			unsigned short slen;
+			unsigned short newline = 0;
+			char last_ch;
+
 			memcpy(&slen, data, 2);
 
-			str = xmalloc(slen + 1);
+			last_ch = *((char *)data + slen + 1);
+			if (last_ch == '\n')
+				newline = 1;
+
+			str = xmalloc(slen + newline + 1);
 			memcpy(str, data + 2, slen);
 			str[slen] = '\0';
+
+			if (newline) {
+				str[slen - 1] = '\\';
+				str[slen]     = 'n';
+				str[slen + 1] = '\0';
+			}
 
 			if (!memcmp(str, &null_str, sizeof(null_str)))
 				n += snprintf(args + n, len, "NULL");
@@ -481,10 +494,10 @@ void get_argspec_string(struct ftrace_task_handle *task,
 				/* quotation mark has to be escaped by backslash
 				   in chrome trace json format */
 				n += snprintf(args + n, len, "\\\"%.*s\\\"",
-					      slen, str);
+					      slen + newline, str);
 			else
 				n += snprintf(args + n, len, "\"%.*s\"",
-					      slen, str);
+					      slen + newline, str);
 
 			free(str);
 			size = slen + 2;
