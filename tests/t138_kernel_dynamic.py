00c826095c84cc420c4cecc7650a10aa90c27d8f
Namhyung Kim <namhyung@gmail.com>
2017-04-04 00:48:45 +0900

    test: Add a test case for dynamic kernel tracing
      $ sudo uftrace -k -P sys_*@kernel tests/t-openclose
      # DURATION    TID     FUNCTION
                  [ 9875] | main() {
                  [ 9875] |   open() {
        14.416 us [ 9875] |     sys_open();
        19.099 us [ 9875] |   } /* open */
                  [ 9875] |   close() {
         3.380 us [ 9875] |     sys_close();
         9.720 us [ 9875] |   } /* close */
        37.051 us [ 9875] | } /* main */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t138_kernel_dynamic.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+import os
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'openclose', """
+# DURATION    TID     FUNCTION
+            [ 9875] | main() {
+            [ 9875] |   open() {
+  14.416 us [ 9875] |     sys_open();
+  19.099 us [ 9875] |   } /* open */
+            [ 9875] |   close() {
+   3.380 us [ 9875] |     sys_close();
+   9.720 us [ 9875] |   } /* close */
+  37.051 us [ 9875] | } /* main */
+""")
+
+    def pre(self):
+        if os.geteuid() != 0:
+            return TestBase.TEST_SKIP
+        if os.path.exists('/.dockerenv'):
+            return TestBase.TEST_SKIP
+
+        return TestBase.TEST_SUCCESS
+
+    def runcmd(self):
+        return '%s -k -P %s %s openclose' % \
+            (TestBase.ftrace, 'sys_*@kernel', 't-' + self.name)
