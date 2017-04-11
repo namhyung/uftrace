19302b2786b3d7ece0f9465fda6205d91f8678b4
Namhyung Kim <namhyung@gmail.com>
2017-04-11 09:59:32 +0900

    arch/x86: Add basic support for SDT event
    The SDT events are inserted with a single-type NOP (0x90), so it needs
    to be replaced to an invalid opcode so that uftrace can catch the SIGILL
    to save the event info.  And it modifies the value of the RIP register
    to proceed with a next instruction after the opcode.
    
    Currently it doesn't save arguments since it requires parsing the format
    and some possible memory dereferences.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ arch/x86_64/mcount-event.c
@@ -0,0 +1,61 @@
+#include <signal.h>
+#include <stdbool.h>
+#include <ucontext.h>
+#include <assert.h>
+#include <sys/mman.h>
+
+/* This should be defined before #include "utils.h" */
+#define PR_FMT     "mcount"
+#define PR_DOMAIN  DBG_MCOUNT
+
+#include "libmcount/mcount.h"
+
+#define INVALID_OPCODE  0xce
+#define PAGE_SIZE       4096
+#define PAGE_ADDR(a)    ((void *)((a) & ~(PAGE_SIZE - 1)))
+
+static void sdt_handler(int sig, siginfo_t *info, void *arg)
+{
+	ucontext_t *ctx = arg;
+	unsigned long addr = ctx->uc_mcontext.gregs[REG_RIP];
+	struct mcount_event_info * mei;
+
+	mei = mcount_lookup_event(addr);
+	assert(mei != NULL);
+
+	/* TODO: collect and write arguments */
+	mcount_write_event(mei);
+
+	/* skip the invalid insn and continue */
+	ctx->uc_mcontext.gregs[REG_RIP] = addr + 1;
+}
+
+int mcount_arch_enable_event(struct mcount_event_info *mei)
+{
+	static bool sdt_handler_set = false;
+
+	if (!sdt_handler_set) {
+		struct sigaction act = {
+			.sa_flags     = SA_SIGINFO,
+			.sa_sigaction = sdt_handler,
+		};
+
+		sigemptyset(&act.sa_mask);
+		sigaction(SIGILL, &act, NULL);
+
+		sdt_handler_set = true;
+	}
+
+	if (mprotect(PAGE_ADDR(mei->addr), PAGE_SIZE, PROT_READ | PROT_WRITE)) {
+		pr_dbg("cannot enable event due to protection: %m\n");
+		return -1;
+	}
+
+	/* replace NOP to an invalid OP so that it can catch SIGILL */
+	memset((void *)mei->addr, INVALID_OPCODE, 1);
+
+	if (mprotect(PAGE_ADDR(mei->addr), PAGE_SIZE, PROT_EXEC))
+		pr_err("cannot setup event due to protection");
+
+	return 0;
+}
