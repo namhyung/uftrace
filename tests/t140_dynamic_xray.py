90f95a1661675d2c0865a67d977440acbb399149
Namhyung Kim <namhyung@gmail.com>
2017-04-26 17:04:22 +0900

    test: Add a test case for xray dynamic tracing
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t140_dynamic_xray.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+import platform
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'abc', """
+# DURATION    TID     FUNCTION
+  62.202 us [28141] | __cxa_atexit();
+            [28141] | main() {
+            [28141] |   a() {
+   0.753 us [28141] |     getpid();
+   2.405 us [28141] |   } /* a */
+   3.005 us [28141] | } /* main */
+""")
+
+    def pre(self):
+        if platform.machine().startswith('arm'):
+            return TestBase.TEST_SKIP
+        return TestBase.TEST_SUCCESS
+
+    def build(self, name, cflags='', ldflags=''):
+        old_cc = TestBase.supported_lang['C']['cc']
+        TestBase.supported_lang['C']['cc'] = 'clang'
+        r = TestBase.build(self, name, '-fxray-instrument -fxray-instruction-threshold=1', ldflags)
+        TestBase.supported_lang['C']['cc'] = old_cc
+        return r
+
+    def runcmd(self):
+        return '%s -P %s %s' % (TestBase.ftrace, 'a.?', 't-' + self.name)
