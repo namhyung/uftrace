21486e2a4e12410efcc342f07fa73acaefbf6ed2
Namhyung Kim <namhyung@gmail.com>
2017-05-22 15:55:51 +0900

    test: Add test cases for recv command
    The recv command is to receivce uftrace data on a socket.  But it used
    to miss some important metadata so add test cases to test it.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t141_recv_basic.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+import subprocess as sp
+
+TDIR  = 'xxx'
+TDIR2 = 'xxx/uftrace.data'
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'abc', """
+# DURATION    TID     FUNCTION
+  62.202 us [28141] | __cxa_atexit();
+            [28141] | main() {
+            [28141] |   a() {
+            [28141] |     b() {
+            [28141] |       c() {
+   0.753 us [28141] |         getpid();
+   1.430 us [28141] |       } /* c */
+   1.915 us [28141] |     } /* b */
+   2.405 us [28141] |   } /* a */
+   3.005 us [28141] | } /* main */
+""")
+
+    recv_p = None
+
+    def pre(self):
+        recv_cmd = '%s recv -d %s' % (TestBase.ftrace, TDIR)
+        self.recv_p = sp.Popen(recv_cmd.split())
+
+        record_cmd = '%s record -H %s %s' % (TestBase.ftrace, 'localhost', 't-abc')
+        sp.call(record_cmd.split())
+        return TestBase.TEST_SUCCESS
+
+    def runcmd(self):
+        return '%s replay -d %s' % (TestBase.ftrace, TDIR2)
+
+    def post(self, ret):
+        self.recv_p.terminate()
+        sp.call(['rm', '-rf', TDIR])
+        return ret
