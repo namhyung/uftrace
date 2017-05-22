namhyung/uftrace

commit cb38d982cc361087e1190215b7ef9a0fd90fa98e

mcount: Staticize startup and cleanup routines
They can be called directly or from init/fini mechanism.  Move the
common code into static functions and avoid calls through PLT.

Signed-off-by: Namhyung Kim <namhyung@gmail.com>

libmcount/mcount.c
@@ -815,142 +815,9 @@ void mcount_rstack_reset(void)
 	}
 }
 
-/*
- * hooking functions
- */
-static int (*real_backtrace)(void **buffer, int sz);
-static void (*real_cxa_throw)(void *exc, void *type, void *dest);
-static void (*real_cxa_end_catch)(void);
-static void * (*real_dlopen)(const char *filename, int flags);
+static void mcount_hook_functions(void);
 
-static void mcount_hook_functions(void)
-{
-	real_backtrace		= dlsym(RTLD_NEXT, "backtrace");
-	real_cxa_throw		= dlsym(RTLD_NEXT, "__cxa_throw");
-	real_cxa_end_catch	= dlsym(RTLD_NEXT, "__cxa_end_catch");
-	real_dlopen		= dlsym(RTLD_NEXT, "dlopen");
-}
-
-__visible_default int backtrace(void **buffer, int sz)
-{
-	int ret;
-
-	if (real_backtrace == NULL)
-		return 0;
-
-	mcount_rstack_restore();
-	ret = real_backtrace(buffer, sz);
-	mcount_rstack_reset();
-
-	return ret;
-}
-
-__visible_default void __cxa_throw(void *exception, void *type, void *dest)
-{
-	struct mcount_thread_data *mtdp;
-
-	/*
-	 * restore return addresses so that it can unwind stack frames
-	 * safely during the exception handling.
-	 * It pairs to __cxa_end_catch().
-	 */
-	mcount_rstack_restore();
-
-	mtdp = get_thread_data();
-	if (!check_thread_data(mtdp))
-		pr_dbg("exception thrown from [%d]\n", mtdp->idx);
-
-	real_cxa_throw(exception, type, dest);
-}
-
-__visible_default void __cxa_end_catch(void)
-{
-	struct mcount_thread_data *mtdp;
-	struct mcount_ret_stack *rstack;
-	unsigned long retaddr;
-
-	/* get frame address where exception handler returns */
-	retaddr = (unsigned long)__builtin_frame_address(0);
-
-	real_cxa_end_catch();
-
-	pr_dbg("exception returned at frame: %#lx\n", retaddr);
-
-	mcount_rstack_restore();
-
-	mtdp = get_thread_data();
-	if (!check_thread_data(mtdp)) {
-		int idx;
-
-		/* it needs to find how much stack frame was unwinded */
-		for (idx = mtdp->idx - 1; idx >= 0; idx--) {
-			rstack = &mtdp->rstack[idx];
-
-			pr_dbg2("[%d] parent at %p\n", idx, rstack->parent_loc);
-			if (rstack->parent_loc == &mtdp->cygprof_dummy)
-				break;
-
-			if ((unsigned long)rstack->parent_loc > retaddr)
-				break;
-
-			/* record unwinded functions */
-			if (!(rstack->flags & MCOUNT_FL_NORECORD))
-				rstack->end_time = mcount_gettime();
-
-			mcount_exit_filter_record(mtdp, rstack, NULL);
-		}
-
-		/* we're in ENTER state, so add 1 to the index */
-		mtdp->idx = idx + 1;
-		pr_dbg("[%d] exception returned\n", mtdp->idx);
-
-		mcount_rstack_reset();
-	}
-}
-
-__visible_default void * dlopen(const char *filename, int flags)
-{
-	struct mcount_thread_data *mtdp;
-	uint64_t timestamp = mcount_gettime();
-	void *ret = real_dlopen(filename, flags);
-	struct dlopen_base_data data = {
-		.libname = simple_basename(filename),
-	};
-
-	if (unlikely(mcount_should_stop()))
-		return ret;
-
-	mtdp = get_thread_data();
-	if (unlikely(check_thread_data(mtdp))) {
-		mtdp = mcount_prepare();
-		if (mtdp == NULL)
-			return ret;
-	}
-	else {
-		if (unlikely(mtdp->recursion_guard))
-			return ret;
-
-		mtdp->recursion_guard = true;
-	}
-
-	dl_iterate_phdr(dlopen_base_callback, &data);
-
-	/*
-	 * get timestamp before calling dlopen() so that
-	 * it can have symbols in static initializers which
-	 * called during the dlopen.
-	 */
-	send_dlopen_msg(mtdp, session_name(), timestamp,
-			data.base_addr, data.libname);
-
-	mtdp->recursion_guard = false;
-	return ret;
-}
-
-/*
- * external interfaces
- */
-void __visible_default __monstartup(unsigned long low, unsigned long high)
+static void mcount_startup(void)
 {
 	char *pipefd_str;
 	char *logfd_str;
@@ -1102,7 +969,7 @@ out:
 	mtd.recursion_guard = false;
 }
 
-void __visible_default _mcleanup(void)
+static void mcount_cleanup(void)
 {
 	mcount_finish();
 	destroy_dynsym_indexes();
@@ -1110,6 +977,151 @@ void __visible_default _mcleanup(void)
 #ifndef DISABLE_MCOUNT_FILTER
 	ftrace_cleanup_filter(&mcount_triggers);
 #endif
+}
+
+/*
+ * hooking functions
+ */
+static int (*real_backtrace)(void **buffer, int sz);
+static void (*real_cxa_throw)(void *exc, void *type, void *dest);
+static void (*real_cxa_end_catch)(void);
+static void * (*real_dlopen)(const char *filename, int flags);
+
+static void mcount_hook_functions(void)
+{
+	real_backtrace		= dlsym(RTLD_NEXT, "backtrace");
+	real_cxa_throw		= dlsym(RTLD_NEXT, "__cxa_throw");
+	real_cxa_end_catch	= dlsym(RTLD_NEXT, "__cxa_end_catch");
+	real_dlopen		= dlsym(RTLD_NEXT, "dlopen");
+}
+
+__visible_default int backtrace(void **buffer, int sz)
+{
+	int ret;
+
+	if (real_backtrace == NULL)
+		return 0;
+
+	mcount_rstack_restore();
+	ret = real_backtrace(buffer, sz);
+	mcount_rstack_reset();
+
+	return ret;
+}
+
+__visible_default void __cxa_throw(void *exception, void *type, void *dest)
+{
+	struct mcount_thread_data *mtdp;
+
+	/*
+	 * restore return addresses so that it can unwind stack frames
+	 * safely during the exception handling.
+	 * It pairs to __cxa_end_catch().
+	 */
+	mcount_rstack_restore();
+
+	mtdp = get_thread_data();
+	if (!check_thread_data(mtdp))
+		pr_dbg("exception thrown from [%d]\n", mtdp->idx);
+
+	real_cxa_throw(exception, type, dest);
+}
+
+__visible_default void __cxa_end_catch(void)
+{
+	struct mcount_thread_data *mtdp;
+	struct mcount_ret_stack *rstack;
+	unsigned long retaddr;
+
+	/* get frame address where exception handler returns */
+	retaddr = (unsigned long)__builtin_frame_address(0);
+
+	real_cxa_end_catch();
+
+	pr_dbg("exception returned at frame: %#lx\n", retaddr);
+
+	mcount_rstack_restore();
+
+	mtdp = get_thread_data();
+	if (!check_thread_data(mtdp)) {
+		int idx;
+
+		/* it needs to find how much stack frame was unwinded */
+		for (idx = mtdp->idx - 1; idx >= 0; idx--) {
+			rstack = &mtdp->rstack[idx];
+
+			pr_dbg2("[%d] parent at %p\n", idx, rstack->parent_loc);
+			if (rstack->parent_loc == &mtdp->cygprof_dummy)
+				break;
+
+			if ((unsigned long)rstack->parent_loc > retaddr)
+				break;
+
+			/* record unwinded functions */
+			if (!(rstack->flags & MCOUNT_FL_NORECORD))
+				rstack->end_time = mcount_gettime();
+
+			mcount_exit_filter_record(mtdp, rstack, NULL);
+		}
+
+		/* we're in ENTER state, so add 1 to the index */
+		mtdp->idx = idx + 1;
+		pr_dbg("[%d] exception returned\n", mtdp->idx);
+
+		mcount_rstack_reset();
+	}
+}
+
+__visible_default void * dlopen(const char *filename, int flags)
+{
+	struct mcount_thread_data *mtdp;
+	uint64_t timestamp = mcount_gettime();
+	void *ret = real_dlopen(filename, flags);
+	struct dlopen_base_data data = {
+		.libname = simple_basename(filename),
+	};
+
+	if (unlikely(mcount_should_stop()))
+		return ret;
+
+	mtdp = get_thread_data();
+	if (unlikely(check_thread_data(mtdp))) {
+		mtdp = mcount_prepare();
+		if (mtdp == NULL)
+			return ret;
+	}
+	else {
+		if (unlikely(mtdp->recursion_guard))
+			return ret;
+
+		mtdp->recursion_guard = true;
+	}
+
+	dl_iterate_phdr(dlopen_base_callback, &data);
+
+	/*
+	 * get timestamp before calling dlopen() so that
+	 * it can have symbols in static initializers which
+	 * called during the dlopen.
+	 */
+	send_dlopen_msg(mtdp, session_name(), timestamp,
+			data.base_addr, data.libname);
+
+	mtdp->recursion_guard = false;
+	return ret;
+}
+
+/*
+ * external interfaces
+ */
+void __visible_default __monstartup(unsigned long low, unsigned long high)
+{
+	mcount_startup();
+}
+
+void __visible_default _mcleanup(void)
+{
+	mcount_cleanup();
 }
 
 void __visible_default mcount_restore(void)
@@ -1139,11 +1151,11 @@ static void __attribute__((constructor))
 mcount_init(void)
 {
 	if (!mcount_setup_done)
-		__monstartup(0UL, ~0UL);
+		mcount_startup();
 }
 
 static void __attribute__((destructor))
 mcount_fini(void)
 {
-	_mcleanup();
+	mcount_cleanup();
 }

commit eb2d5070f9cf91cc0613ec2d1961cc7c61bc743a

mcount: Only call mcount_{startup,cleanup}() one time
Currently mcount_{startup,cleanup}() are respectively called twice,
so omit to duplicately call them in __monstartup() and _mcleanup().
And mcount_setup_done already checked in mcount_startup(),
so remove the duplicated code checking it.

Fixes: cb38d98 ("mcount: Staticize startup and cleanup routines")

Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

libmcount/mcount.c
@@ -1236,12 +1236,10 @@ __visible_default void * dlopen(const char *filename, int flags)
  */
 void __visible_default __monstartup(unsigned long low, unsigned long high)
 {
-	mcount_startup();
 }
 
 void __visible_default _mcleanup(void)
 {
-	mcount_cleanup();
 }
 
 void __visible_default mcount_restore(void)
@@ -1271,8 +1269,7 @@ void __visible_default __cyg_profile_func_exit(void *child, void *parent)
 static void __attribute__((constructor))
 mcount_init(void)
 {
-	if (!mcount_setup_done)
-		mcount_startup();
+	mcount_startup();
 }
 
 static void __attribute__((destructor))