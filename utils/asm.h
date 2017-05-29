6680ddcf75c638a9b8baa66b51685f5cc0524a55
Namhyung Kim <namhyung@gmail.com>
2017-05-29 13:50:34 +0900

    mcount: Introduce utils/asm.h
    It provides ENTRY() and END() macro for assembly source to define
    global functions.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ utils/asm.h
@@ -0,0 +1,12 @@
+#ifndef __UFTRACE_ASM_H__
+#define __UFTRACE_ASM_H__
+
+#define ENTRY(sym)				\
+	.global sym;				\
+	.type sym, %function;			\
+sym:
+
+#define END(sym)				\
+	.size sym, .-sym;
+
+#endif /* __UFTRACE_ASM_H__ */
