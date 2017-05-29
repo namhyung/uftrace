7f4ea10b30b5e903270adb99690f50992b73422e
Namhyung Kim <namhyung@gmail.com>
2017-05-29 14:03:31 +0900

    mcount: Add an alias to global mcount symbols
    The mcount and its friends are to be called from the target binary.  But
    libmcount also needs to take their address without intercepted by
    others (the binary).  So add a hidden symbol as an alias and use the
    address instead.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/asm.h
+++ utils/asm.h
@@ -4,7 +4,11 @@
 #define GLOBAL(sym)				\
 	.global sym;				\
 	.type sym, %function;			\
-sym:
+sym:						\
+	.global uftrace_ ## sym;		\
+	.hidden uftrace_ ## sym;		\
+	.type uftrace_ ## sym, %function;	\
+uftrace_ ## sym:
 
 #define ENTRY(sym)				\
 	.global sym;				\
