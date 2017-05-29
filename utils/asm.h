6cfb5093d7587a8bf8cbf6d6a1baa0e6685435dd
Namhyung Kim <namhyung@gmail.com>
2017-05-29 13:54:46 +0900

    mcount: Add GLOBAL() macro for global definitions in asm
    Convert existing ENTRY() macro to define hidden symbols (not exported)
    and use GLOBAL() maro for externally-visible symbols only.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/asm.h
+++ utils/asm.h
@@ -1,8 +1,14 @@
 #ifndef __UFTRACE_ASM_H__
 #define __UFTRACE_ASM_H__
 
+#define GLOBAL(sym)				\
+	.global sym;				\
+	.type sym, %function;			\
+sym:
+
 #define ENTRY(sym)				\
 	.global sym;				\
+	.hidden sym;				\
 	.type sym, %function;			\
 sym:
 
