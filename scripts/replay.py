92d2e2998bbf12ee4eab8e14fe6d7d674c7ab283
Honggyu Kim <honggyu.kp@gmail.com>
2017-06-16 23:40:47 +0900

    script: Add replay.py python script
    This adds a new python script, replay.py, in scripts directory.
    The usage of the script is as follows:
    
    The below example shows replay output in record time and it almost looks
    like 'uftrace replay' output.
    
      $ uftrace record -S scripts/replay.py -F main tests/t-abc
      # DURATION    TID     FUNCTION
                  [25794] | main() {
                  [25794] |   a() {
                  [25794] |     b() {
                  [25794] |       c() {
                  [25794] |         getpid() {
        11.037 us [25794] |         } /* getpid */
        44.752 us [25794] |       } /* c */
        70.924 us [25794] |     } /* b */
        98.191 us [25794] |   } /* a */
       124.329 us [25794] | } /* main */
    
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

---
+++ scripts/replay.py
@@ -0,0 +1,47 @@
+print("# DURATION    TID     FUNCTION")
+
+def uftrace_entry(args):
+    # read arguments
+    _tid = args["tid"]
+    _depth = args["depth"]
+    _symname = args["symname"]
+
+    indent = _depth * 2
+    space = " " * indent
+
+    buf = " %10s [%5d] | %s%s() {" % ("", _tid, space, _symname)
+    print(buf)
+
+def uftrace_exit(args):
+    # read arguments
+    _tid = args["tid"]
+    _depth = args["depth"]
+    _symname = args["symname"]
+    _duration = args["duration"]
+
+    indent = _depth * 2
+    space = " " * indent
+
+    (time, unit) = get_time_and_unit(_duration)
+    buf = " %7.3f %s [%5d] | %s}" % (time, unit, _tid, space)
+    buf = "%s /* %s */" % (buf, _symname)
+    print(buf)
+
+def get_time_and_unit(duration):
+    duration = float(duration)
+    time_unit = ""
+
+    if duration < 100:
+        divider = 1
+        time_unit = "ns"
+    elif duration < 1000000:
+        divider = 1000
+        time_unit = "us"
+    elif duration < 1000000000:
+        divider = 1000000
+        time_unit = "ms"
+    else:
+        divider = 1000000000
+        time_unit = " s"
+
+    return (duration / divider, time_unit)
