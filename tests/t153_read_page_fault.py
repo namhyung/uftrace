c886ca9c6ba753818477ad3cb31f60b518a38994
Namhyung Kim <namhyung@gmail.com>
2017-07-12 14:02:53 +0900

    test: Add test cases for read trigger
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t153_read_page_fault.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'abc', """
+# DURATION    TID     FUNCTION
+            [32766] | main() {
+            [32766] |   a() {
+            [32766] |     b() {
+            [32766] |       /* read:page-fault (major=0, minor=188) */
+            [32766] |       c() {
+   0.609 us [32766] |         getpid();
+  13.722 us [32766] |       } /* c */
+  24.950 us [32766] |     } /* b */
+  25.564 us [32766] |   } /* a */
+  26.963 us [32766] | } /* main */
+""")
+
+    def runcmd(self):
+        uftrace = TestBase.ftrace
+        args    = '-F main -T b@read=page-fault'
+        prog    = 't-' + self.name
+        return '%s %s %s' % (uftrace, args, prog)
+
+    def sort(self, output):
+        result = []
+        for ln in output.split('\n'):
+            # ignore blank lines and comments
+            if ln.strip() == '' or ln.startswith('#'):
+                continue
+            func = ln.split('|', 1)[-1]
+            # remove actual numbers in page-fault
+            if func.find('read:page-fault') > 0:
+                func = '       /* read:page-fault */'
+            result.append(func)
+
+        return '\n'.join(result)
