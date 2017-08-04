caf2c2831541793c164a977ec61b24a0b070ccc2
Namhyung Kim <namhyung@gmail.com>
2017-08-04 10:19:34 +0900

    misc: Update debug script
    It can use --keep-pid option to following current task (without
    using follow-fork-mode child).  Also it'd be better breaking at exec and
    install new breakpoints there.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- misc/debug-mcount.cmd
+++ misc/debug-mcount.cmd
@@ -1,12 +1,13 @@
 file uftrace
 set breakpoint pending on
 
-b command_record
+#b command_record
+catch exec
 commands
-  set follow-fork-mode child
+#  set follow-fork-mode child
   b main
   continue
 end
 
-r record -L. -d xxx tests/t-abc
+r record -L. -d xxx --keep-pid --force tests/t-abc
  
