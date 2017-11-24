2600756729222ccac809ad07082ed3de7c12e936
Namhyung Kim <namhyung@gmail.com>
2017-11-24 17:30:39 +0900

    graph: Move generic graph routines to utils/graph.c
    The code to build function call graph can be shared.  Currently graph
    command and dump --flame-graph both uses same kind of graph.  Let's
    share the code and remove the duplication.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ utils/graph.c
@@ -0,0 +1,211 @@
+#include "utils/graph.h"
+#include "utils/list.h"
+#include "utils/rbtree.h"
+#include "utils/filter.h"
+
+static graph_fn entry_cb;
+static graph_fn exit_cb;
+static graph_fn event_cb;
+static void *cb_arg;
+
+static struct rb_root task_graph_root = RB_ROOT;
+
+void graph_init(struct uftrace_graph *graph, struct uftrace_session *s)
+{
+	memset(graph, 0, sizeof(*graph));
+	graph->sess = s;
+
+	INIT_LIST_HEAD(&graph->root.head);
+	INIT_LIST_HEAD(&graph->special_nodes);
+}
+
+void graph_init_callbacks(graph_fn entry_fn, graph_fn exit_fn, graph_fn event_fn, void *arg)
+{
+	entry_cb = entry_fn;
+	exit_cb  = exit_fn;
+	event_cb = event_fn;
+
+	cb_arg   = arg;
+}
+
+struct uftrace_task_graph * graph_get_task(struct ftrace_task_handle *task,
+					   size_t tg_size)
+{
+	struct rb_node *parent = NULL;
+	struct rb_node **p = &task_graph_root.rb_node;
+	struct uftrace_task_graph *tg;
+
+	while (*p) {
+		parent = *p;
+		tg = rb_entry(parent, struct uftrace_task_graph, link);
+
+		if (tg->task->tid == task->tid)
+			return tg;
+
+		if (tg->task->tid > task->tid)
+			p = &parent->rb_left;
+		else
+			p = &parent->rb_right;
+	}
+
+	tg = xzalloc(tg_size);
+	tg->task = task;
+
+	rb_link_node(&tg->link, parent, p);
+	rb_insert_color(&tg->link, &task_graph_root);
+
+	return tg;
+}
+
+static int add_graph_entry(struct uftrace_task_graph *tg)
+{
+	struct uftrace_graph_node *node = NULL;
+	struct uftrace_graph_node *curr = tg->node;
+	struct uftrace_record *rstack = tg->task->rstack;
+
+	if (tg->lost)
+		return 1;  /* ignore kernel functions after LOST */
+
+	if (tg->new_sess) {
+		curr = &tg->graph->root;
+		pr_dbg2("starts new session graph for task %d\n", tg->task->tid);
+		tg->new_sess = false;
+	}
+
+	if (curr == NULL)
+		return -1;
+
+	list_for_each_entry(node, &curr->head, list) {
+		if (node->addr == rstack->addr)
+			break;
+	}
+
+	if (list_no_entry(node, &curr->head, list)) {
+		struct uftrace_trigger tr;
+		struct uftrace_session *sess = tg->graph->sess;
+
+		node = xcalloc(1, sizeof(*node));
+
+		node->addr = rstack->addr;
+		INIT_LIST_HEAD(&node->head);
+
+		node->parent = curr;
+		list_add_tail(&node->list, &node->parent->head);
+		node->parent->nr_edges++;
+
+		if (uftrace_match_filter(node->addr, &sess->fixups, &tr)) {
+			struct sym *sym;
+			struct uftrace_special_node *snode;
+			enum uftrace_graph_node_type type = NODE_T_NORMAL;
+
+			sym = find_symtabs(&sess->symtabs, node->addr);
+			if (sym == NULL)
+				goto out;
+
+			if (!strcmp(sym->name, "fork") ||
+			    !strcmp(sym->name, "vfork") ||
+			    !strcmp(sym->name, "daemon"))
+				type = NODE_T_FORK;
+			else if (!strncmp(sym->name, "exec", 4))
+				type = NODE_T_EXEC;
+			else
+				goto out;
+
+			snode = xmalloc(sizeof(*snode));
+			snode->node = node;
+			snode->type = type;
+			snode->pid  = tg->task->t->pid;
+
+			/* find recent one first */
+			list_add(&snode->list, &tg->graph->special_nodes);
+		}
+	}
+
+out:
+	node->nr_calls++;
+	tg->node = node;
+
+	if (entry_cb)
+		entry_cb(tg, cb_arg);
+
+	return 0;
+}
+
+static int add_graph_exit(struct uftrace_task_graph *tg)
+{
+	struct fstack *fstack = &tg->task->func_stack[tg->task->stack_count];
+	struct uftrace_graph_node *node = tg->node;
+
+	if (node == NULL)
+		return -1;
+
+	if (tg->lost) {
+		if (is_kernel_address(&tg->task->h->sessions.first->symtabs,
+				      fstack->addr))
+			return 1;
+
+		/*
+		 * LOST only occures in kernel, so clear tg->lost
+		 * when return to userspace
+		 */
+		tg->lost = false;
+	}
+
+	if (node->addr != fstack->addr) {
+		struct uftrace_special_node *snode, *tmp;
+
+		list_for_each_entry_safe(snode, tmp, &tg->graph->special_nodes, list) {
+			if (snode->node->addr == tg->task->rstack->addr &&
+			    snode->type == NODE_T_FORK &&
+			    snode->pid == tg->task->t->ppid) {
+				node = snode->node;
+				list_del(&snode->list);
+				pr_dbg("recover from fork\n");
+				goto out;
+			}
+		}
+		pr_dbg("broken graph - addresses not match\n");
+	}
+
+out:
+	node->time       += fstack->total_time;
+	node->child_time += fstack->child_time;
+
+	if (exit_cb)
+		exit_cb(tg, cb_arg);
+
+	tg->node = node->parent;
+
+	return 0;
+}
+
+static int add_graph_event(struct uftrace_task_graph *tg)
+{
+	struct uftrace_record *rec = tg->task->rstack;
+
+	if (event_cb)
+		event_cb(tg, cb_arg);
+
+	if (rec->addr == EVENT_ID_PERF_SCHED_OUT) {
+		/* to match addr with sched-in */
+		rec->addr = EVENT_ID_PERF_SCHED_IN;
+		return add_graph_entry(tg);
+	}
+	else if (rec->addr == EVENT_ID_PERF_SCHED_IN) {
+		return add_graph_exit(tg);
+	}
+
+	return -1;
+}
+
+int graph_add_node(struct uftrace_task_graph *tg, int type)
+{
+	if (type == UFTRACE_ENTRY)
+		return add_graph_entry(tg);
+	else if (type == UFTRACE_EXIT)
+		return add_graph_exit(tg);
+	else if (type == UFTRACE_EVENT)
+		return add_graph_event(tg);
+	else
+		return 0;
+}
---
+++ utils/graph.h
@@ -0,0 +1,61 @@
+#ifndef UFTRACE_GRAPH_H
+#define UFTRACE_GRAPH_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "uftrace.h"
+#include "utils/list.h"
+#include "utils/rbtree.h"
+#include "utils/fstack.h"
+
+struct uftrace_graph_node {
+	uint64_t			addr;
+	int				nr_edges;
+	int				nr_calls;
+	uint64_t			time;
+	uint64_t			child_time;
+	struct list_head		head;
+	struct list_head		list;
+	struct uftrace_graph_node	*parent;
+};
+
+enum uftrace_graph_node_type {
+	NODE_T_NORMAL,
+	NODE_T_FORK,
+	NODE_T_EXEC,
+};
+
+struct uftrace_special_node {
+	struct list_head		list;
+	struct uftrace_graph_node	*node;
+	enum uftrace_graph_node_type	type;
+	int				pid;
+};
+
+struct uftrace_graph {
+	bool				kernel_only;
+	struct uftrace_session		*sess;
+	struct list_head		special_nodes;
+	struct uftrace_graph_node	root;
+};
+
+struct uftrace_task_graph {
+	bool				lost;
+	bool				new_sess;
+	struct ftrace_task_handle	*task;
+	struct uftrace_graph		*graph;
+	struct uftrace_graph_node	*node;
+	struct rb_node			link;
+};
+
+typedef void(*graph_fn)(struct uftrace_task_graph *tg, void *arg);
+
+void graph_init(struct uftrace_graph *graph, struct uftrace_session *s);
+void graph_init_callbacks(graph_fn entry, graph_fn exit, graph_fn event,
+			  void *arg);
+struct uftrace_task_graph * graph_get_task(struct ftrace_task_handle *task,
+					   size_t tg_size);
+int graph_add_node(struct uftrace_task_graph *tg, int type);
+
+#endif /* UFTRACE_GRAPH_H */
