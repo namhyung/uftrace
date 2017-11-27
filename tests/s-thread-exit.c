448e3c26275ddccaaa6873f1847ee3f18d90cf20
Namhyung Kim <namhyung@gmail.com>
2017-11-27 09:45:55 +0900

    test: Add a test case for pthread_exit()
    Strangely, calling pthread_exit() after printf() with floating-point
    argument caused a segfault on some systems.  Add a test case for it.
    Thanks to @minchae for reporting issue with a test program.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/s-thread-exit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREAD 2
+
+pthread_t threads[NUM_THREAD];
+
+void *thread_main(void *arg)
+{
+	double result = 1.0;
+
+	printf("%f\n",result);
+	pthread_exit(NULL);
+	return NULL;
+}
+
+int main(void)
+{
+	for (int i = 0; i < NUM_THREAD; i++)
+		pthread_create(&threads[i], NULL, &thread_main, NULL);
+
+	for (int i = 0; i < NUM_THREAD; i++)
+		pthread_join(threads[i], NULL);
+
+	return 0;
+}
