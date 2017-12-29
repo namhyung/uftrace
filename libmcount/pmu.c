5f3c141aa8f31755d378e089e02bca07ca2c2a43
Namhyung Kim <namhyung@gmail.com>
2017-12-29 11:50:25 +0900

    mcount: Read perf events data as group
    The perf syscall provides a group read functionality to reduce
    overhead.  A group can be created by passing group_fd to the
    perf_event_open() syscall.  A group of event will be scheduled at once
    so their numbers are meaningful to get a ratio.
    
    In order to read group event at once, PERF_FORMAT_GROUP should be set
    and read using a appropriate buffer (including number of group members).
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- libmcount/pmu.c
+++ libmcount/pmu.c
@@ -20,28 +20,24 @@ struct pmu_data {
 
 static LIST_HEAD(pmu_fds);
 
-static int open_perf_event(uint32_t type, uint64_t config)
+static int open_perf_event(uint32_t type, uint64_t config, int group_fd)
 {
 	struct perf_event_attr attr = {
 		.size			= sizeof(attr),
 		.type			= type,
 		.config			= config,
 		.exclude_kernel		= 1,
-		.inherit		= 1,
+		.read_format		= PERF_FORMAT_GROUP,
 	};
-	unsigned long flag = PERF_FLAG_FD_NO_GROUP;
+	unsigned long flag = PERF_FLAG_FD_CLOEXEC;
 
-	return syscall(SYS_perf_event_open, &attr, 0, -1, -1, flag);
+	return syscall(SYS_perf_event_open, &attr, 0, -1, group_fd, flag);
 }
 
-static uint64_t read_perf_event(int fd)
+static void read_perf_event(int fd, void *buf, ssize_t len)
 {
-	uint64_t value;
-
-	if (read(fd, &value, sizeof(value)) != sizeof(value))
-		value = -1ULL;
-
-	return value;
+	if (read(fd, buf, len) != len)
+		pr_dbg("reading perf_event failed: %m\n");
 }
 
 int prepare_pmu_event(enum uftrace_event_id id)
@@ -61,7 +57,8 @@ int prepare_pmu_event(enum uftrace_event_id id)
 		pd->evt_id = id;
 
 		pd->fd[0] = open_perf_event(PERF_TYPE_HARDWARE,
-					    PERF_COUNT_HW_CPU_CYCLES);
+					    PERF_COUNT_HW_CPU_CYCLES,
+					    -1);
 		if (pd->fd[0] < 0) {
 			pr_warn("failed to open 'cpu-cycles' perf event: %m\n");
 			free(pd);
@@ -69,7 +66,8 @@ int prepare_pmu_event(enum uftrace_event_id id)
 		}
 
 		pd->fd[1] = open_perf_event(PERF_TYPE_HARDWARE,
-					    PERF_COUNT_HW_INSTRUCTIONS);
+					    PERF_COUNT_HW_INSTRUCTIONS,
+					    pd->fd[0]);
 		if (pd->fd[1] < 0) {
 			pr_warn("failed to open 'instructions' perf event: %m\n");
 			close(pd->fd[0]);
@@ -83,7 +81,8 @@ int prepare_pmu_event(enum uftrace_event_id id)
 		pd->evt_id = id;
 
 		pd->fd[0] = open_perf_event(PERF_TYPE_HARDWARE,
-					    PERF_COUNT_HW_CACHE_REFERENCES);
+					    PERF_COUNT_HW_CACHE_REFERENCES,
+					    -1);
 		if (pd->fd[0] < 0) {
 			pr_warn("failed to open 'cache-references' perf event: %m\n");
 			free(pd);
@@ -91,7 +90,8 @@ int prepare_pmu_event(enum uftrace_event_id id)
 		}
 
 		pd->fd[1] = open_perf_event(PERF_TYPE_HARDWARE,
-					    PERF_COUNT_HW_CACHE_MISSES);
+					    PERF_COUNT_HW_CACHE_MISSES,
+					    pd->fd[0]);
 		if (pd->fd[1] < 0) {
 			pr_warn("failed to open 'cache-misses' perf event: %m\n");
 			close(pd->fd[0]);
@@ -114,6 +114,10 @@ int read_pmu_event(enum uftrace_event_id id, void *buf)
 	struct pmu_data *pd;
 	struct uftrace_pmu_cycle *cycle;
 	struct uftrace_pmu_cache *cache;
+	struct {
+		uint64_t	nr_members;
+		uint64_t	data[2];
+	} read_buf;
 
 	list_for_each_entry(pd, &pmu_fds, list) {
 		if (pd->evt_id == id)
@@ -127,14 +131,20 @@ int read_pmu_event(enum uftrace_event_id id, void *buf)
 
 	switch (id) {
 	case EVENT_ID_READ_PMU_CYCLE:
+		/* read group events at once */
+		read_perf_event(pd->fd[0], &read_buf, sizeof(read_buf));
+
 		cycle = buf;
-		cycle->cycles = read_perf_event(pd->fd[0]);
-		cycle->instrs = read_perf_event(pd->fd[1]);
+		cycle->cycles = read_buf.data[0];
+		cycle->instrs = read_buf.data[1];
 		break;
 	case EVENT_ID_READ_PMU_CACHE:
+		/* read group events at once */
+		read_perf_event(pd->fd[0], &read_buf, sizeof(read_buf));
+
 		cache = buf;
-		cache->refers = read_perf_event(pd->fd[0]);
-		cache->misses = read_perf_event(pd->fd[1]);
+		cache->refers = read_buf.data[0];
+		cache->misses = read_buf.data[1];
 		break;
 	default:
 		break;
