d4056704e9c0385bfa3bd66ac7f55af098a0cf57
Namhyung Kim <namhyung@gmail.com>
2018-01-03 14:39:15 +0900

    test: Add a test case for posix_spawn()
      $ uftrace tests/t-posix_spawn
      # DURATION     TID     FUNCTION
                  [ 20512] | main() {
       217.145 us [ 20512] |   posix_spawn();
                  [ 20512] |   waitpid() {
                  [ 20516] | main() {
                  [ 20516] |   a() {
                  [ 20516] |     b() {
                  [ 20516] |       c() {
         0.846 us [ 20516] |         getpid();
         3.798 us [ 20516] |       } /* c */
         4.209 us [ 20516] |     } /* b */
         4.514 us [ 20516] |   } /* a */
         5.223 us [ 20516] | } /* main */
        18.446 ms [ 20512] |   } /* waitpid */
       198.914 us [ 20512] |   posix_spawn();
                  [ 20512] |   waitpid() {
                  [ 20517] | main() {
         7.903 us [ 20517] |   fopen();
         6.671 us [ 20517] |   fclose();
        19.944 us [ 20517] | } /* main */
        31.908 ms [ 20512] |   } /* waitpid */
        50.784 ms [ 20512] | } /* main */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/s-posix_spawn.c
@@ -0,0 +1,27 @@
+/*
+ * This test calls posix_spawn() to run the t-abc and t-openclose executables.
+ */
+#include <stdio.h>
+#include <spawn.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define TEST_PROG1  "t-abc"
+#define TEST_PROG2  "t-openclose"
+
+int main(int argc, char *argv[])
+{
+	int pid;
+	char *args[] = { NULL, NULL, };
+	char *envp[] = { "PATH=.", "HOME=/home/user", NULL };
+
+	args[0] = TEST_PROG1;
+	posix_spawn(&pid, TEST_PROG1, NULL, NULL, args, envp);
+	waitpid(pid, NULL, 0);
+
+	args[0] = TEST_PROG2;
+	posix_spawn(&pid, TEST_PROG2, NULL, NULL, args, envp);
+	waitpid(pid, NULL, 0);
+
+	return 0;
+}
