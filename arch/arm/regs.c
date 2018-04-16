672121b0675a5abe086520036600f03417fc4d45
Namhyung Kim <namhyung@gmail.com>
2018-04-16 16:09:25 +0900

    arch/arm: Implement arch_register_dwarf_name()
    It translates dwarf register op number to name.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- arch/arm/regs.c
+++ arch/arm/regs.c
@@ -2,10 +2,11 @@
 #include "utils/utils.h"
 
 struct arm_reg_table {
-	const char		*name;
-	enum arm_reg_index	idx;
-} reg_table[] = {
+	const char	*name;
+	int		idx;
+};
 
+static const struct arm_reg_table reg_table[] = {
 #define ARM_REG(_r)  { #_r, ARM_REG_##_r }
 
 	/* integer registers */
@@ -31,3 +32,37 @@ int arch_register_index(char *reg_name)
 	}
 	return -1;
 }
+
+#ifdef HAVE_LIBDW
+
+#include <dwarf.h>
+
+#define ARM_REG_VFPv3_BASE  256
+static const struct x86_reg_table dwarf_table[] = {
+	/* support registers used for arguments */
+	{ "r0", DW_OP_reg0, },
+	{ "r1", DW_OP_reg1, },
+	{ "r2", DW_OP_reg2, },
+	{ "r3", DW_OP_reg3, },
+	{ "d0", ARM_REG_VFPv3_BASE + 0, },
+	{ "d1", ARM_REG_VFPv3_BASE + 1, },
+	{ "d2", ARM_REG_VFPv3_BASE + 2, },
+	{ "d3", ARM_REG_VFPv3_BASE + 3, },
+	{ "d4", ARM_REG_VFPv3_BASE + 4, },
+	{ "d5", ARM_REG_VFPv3_BASE + 5, },
+	{ "d6", ARM_REG_VFPv3_BASE + 6, },
+	{ "d7", ARM_REG_VFPv3_BASE + 7, },
+};
+
+const char * arch_register_dwarf_name(int dwarf_reg)
+{
+	unsigned i;
+
+	for (i = 0; i < ARRAY_SIZE(dwarf_table); i++) {
+		if (dwarf_reg == dwarf_table[i].idx)
+			return dwarf_table[i].name;
+	}
+	return "invalid register";
+}
+
+#endif /* HAVE_LIBDW */
