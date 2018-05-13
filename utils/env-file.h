b763130a48e4d510565968d8f2d83b962dd79856
HanbumPark <kese111@gmail.com>
2018-05-13 16:37:35 +0900

    utils: Add 'env-file' to support sharing environment based on file.
    by default, 'uftrace' use environment variable to share options with
    target program. because target program be forked from 'uftrace',
    target program can be share the environment variable. but this
    approach is no longer possible when use dynamic trace because there
    is no way to share environment variable between separated process.
    therefore, extracted command-line options into a file to share with
    the target program. 'env-file' contain some useful function to handle
    that file.
    
    Signed-off-by: HanbumPark <kese111@gmail.com>

---
+++ utils/env-file.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include "utils/utils.h"
+
+#define ENV_FILE  "/tmp/uftrace_environ_file"
+
+int create_env_file();
+int open_env_file();
+void set_env_to_file(int fd, char* key, char* value);
+void set_env_from_file(int fd);
