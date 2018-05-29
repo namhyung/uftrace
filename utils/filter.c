1d5896102587d55fe44060223d790fe1d13b5d42
Namhyung Kim <namhyung@gmail.com>
2018-05-30 00:12:34 +0900

    trigger: Ignore location info after enum name
    DWARF debug info generates argspec with enum and location info like
    below.  It resulted in enum lookup failure so let's get rid of it.
    
      foo@arg1/e:bar%rdi  -> foo@arg1/e:bar
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/filter.c
+++ utils/filter.c
@@ -420,6 +420,7 @@ static int parse_spec(char *str, struct uftrace_arg_spec *arg, char *suffix)
 	int size = sizeof(long);
 	int type = arg->type;
 	int bit;
+	char *p;
 
 	if (suffix == NULL || *suffix == '\0')
 		goto out;
@@ -475,6 +476,10 @@ static int parse_spec(char *str, struct uftrace_arg_spec *arg, char *suffix)
 			return -1;
 		}
 		arg->enum_str = xstrdup(&suffix[2]);
+
+		p = strchr(arg->enum_str, '%');
+		if (p)
+			*p = '\0';
 		pr_dbg2("parsing argspec for enum: %s\n", arg->enum_str);
 		goto out;
 	default:
