b34a9758656595d86ba0255ad0f3f1d5f999dfe3
Honggyu Kim <honggyu.kp@gmail.com>
2018-06-22 12:07:17 +0900

    utils: Remove bold attribute to some colors
    Some terminals implement bold as a different color, so bold colors may
    look differently depending on terminal settings for each environment.
    
    So this patch removes bold attributes and makes those to bright colors.
    
    Link: https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
    Suggested-by: Namhyung Kim <namhyung@gmail.com>
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- utils/debug.c
+++ utils/debug.c
@@ -18,10 +18,10 @@
 #define TERM_COLOR_NORMAL	""
 #define TERM_COLOR_RESET	"\033[0m"
 #define TERM_COLOR_BOLD		"\033[1m"
-#define TERM_COLOR_RED		"\033[1;31m"  /* bright red */
+#define TERM_COLOR_RED		"\033[91m"    /* bright red */
 #define TERM_COLOR_GREEN	"\033[32m"
 #define TERM_COLOR_YELLOW	"\033[33m"
-#define TERM_COLOR_BLUE		"\033[1;34m"  /* bright blue */
+#define TERM_COLOR_BLUE		"\033[94m"    /* bright blue */
 #define TERM_COLOR_MAGENTA	"\033[35m"
 #define TERM_COLOR_CYAN		"\033[36m"
 #define TERM_COLOR_GRAY		"\033[90m"    /* bright black */
