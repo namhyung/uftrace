4a9b99e2ca2ca09801e5dad7a21d96ce14ba7d59
GwanYeong Kim <gy741.kim@gmail.com>
2018-10-15 11:36:06 +0900

    utils: Fix crash in clear_uftrace_info()
    uftrace crashes when using an empty directory.
    To protect from it, check the before calling clear_uftrace_info().
    
    Fixed : #516
    
    Signed-off-by: GwanYeong Kim <gy741.kim@gmail.com>

--- utils/data-file.c
+++ utils/data-file.c
@@ -417,6 +417,8 @@ int open_data_file(struct opts *opts, struct ftrace_file_handle *handle)
 	if (fp != NULL)
 		goto ok;
 
+	saved_errno = errno;
+
 	/* if default dirname is failed */
 	if (!strcmp(opts->dirname, UFTRACE_DIR_NAME)) {
 		/* try again inside the current directory */
