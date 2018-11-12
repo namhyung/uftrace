da67431b4d9d3f9868624fe15fa020d910a00998
Namhyung Kim <namhyung@gmail.com>
2018-11-12 20:40:15 +0900

    arch/x86: Load dynamic symbols without PLT
    Parse .rela.dyn section to load symbols with R_X86_64_GLOB_DAT reloc.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- arch/x86_64/symbol.c
+++ arch/x86_64/symbol.c
@@ -119,3 +119,82 @@ int arch_load_dynsymtab_bindnow(struct symtab *dsymtab,
 out:
 	return ret;
 }
+
+int arch_load_dynsymtab_noplt(struct symtab *dsymtab,
+			      struct uftrace_elf_data *elf,
+			      unsigned long offset, unsigned long flags)
+{
+	struct uftrace_elf_iter sec_iter;
+	struct uftrace_elf_iter rel_iter;
+	struct uftrace_elf_iter sym_iter;
+	unsigned grow = SYMTAB_GROW;
+	unsigned long reloc_start = 0;
+	size_t reloc_entsize = 0;
+
+	memset(dsymtab, 0, sizeof(*dsymtab));
+
+	/* assumes there's only one RELA section (rela.dyn) for no-plt binary */
+	elf_for_each_shdr(elf, &sec_iter) {
+		if (sec_iter.shdr.sh_type == SHT_RELA) {
+			memcpy(&rel_iter, &sec_iter, sizeof(sec_iter));
+			pr_dbg2("found RELA section: %s\n",
+				elf_get_name(elf, &sec_iter, sec_iter.shdr.sh_name));
+
+			reloc_start = rel_iter.shdr.sh_offset + offset;
+			reloc_entsize = rel_iter.shdr.sh_entsize;
+		}
+		else if (sec_iter.shdr.sh_type == SHT_DYNSYM) {
+			memcpy(&sym_iter, &sec_iter, sizeof(sec_iter));
+			elf_get_strtab(elf, &sym_iter, sec_iter.shdr.sh_link);
+			elf_get_secdata(elf, &sym_iter);
+		}
+	}
+
+	elf_for_each_rela(elf, &rel_iter) {
+		struct sym *sym;
+		int symidx;
+		char *name;
+
+		symidx = elf_rel_symbol(&rel_iter.rela);
+		if (symidx == 0)
+			continue;
+
+		if (elf_rel_type(&rel_iter.rela) != R_X86_64_GLOB_DAT)
+			continue;
+
+		elf_get_symbol(elf, &sym_iter, symidx);
+
+		if (elf_symbol_type(&sym_iter.sym) != STT_FUNC &&
+		    elf_symbol_type(&sym_iter.sym) != STT_GNU_IFUNC)
+			continue;
+
+		if (sym_iter.sym.st_shndx != STN_UNDEF)
+			continue;
+
+		if (dsymtab->nr_sym >= dsymtab->nr_alloc) {
+			if (dsymtab->nr_alloc >= grow * 4)
+				grow *= 2;
+			dsymtab->nr_alloc += grow;
+			dsymtab->sym = xrealloc(dsymtab->sym,
+						dsymtab->nr_alloc * sizeof(*sym));
+		}
+
+		sym = &dsymtab->sym[dsymtab->nr_sym++];
+
+		/* use reloc address as symbol address as it's in the map */
+		sym->addr = reloc_start + rel_iter.i * reloc_entsize;
+		sym->size = reloc_entsize;
+		sym->type = ST_PLT;
+
+		name = elf_get_name(elf, &sym_iter, sym_iter.sym.st_name);
+		if (flags & SYMTAB_FL_DEMANGLE)
+			sym->name = demangle(name);
+		else
+			sym->name = xstrdup(name);
+
+		pr_dbg3("[%zd] %c %lx + %-5u %s\n", dsymtab->nr_sym,
+			sym->type, sym->addr, sym->size, sym->name);
+	}
+
+	return dsymtab->nr_sym;
+}
