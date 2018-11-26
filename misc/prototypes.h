5150e3c44510754ee0a0ab5179d043d29a9f92d6
Honggyu Kim <honggyu.kp@gmail.com>
2018-11-26 19:47:46 +0900

    misc: Do not return the same pointer uselessly
    Since the following functions return the same pointer values as their
    arguments, it's better not to record their return value.
    
      void memcpy(void *dest, const void *src, size_t n);
      void memset(void *s, int c, size_t n);
      void memmove(void *dest, const void *src, size_t n);
    
    Suggested-by: Namhyung Kim <namhyung@gmail.com>
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- misc/prototypes.h
+++ misc/prototypes.h
@@ -136,10 +136,10 @@ size_t strspn(const char *s, const char *accept);
 size_t strcspn(const char *s, const char *reject);
 char* strsep(char **stringp, const char *delim);
 
-void *memcpy(void *dest, const void *src, size_t n);
-void *memset(void *s, int c, size_t n);
+void memcpy(void *dest, const void *src, size_t n);
+void memset(void *s, int c, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
-void *memmove(void *dest, const void *src, size_t n);
+void memmove(void *dest, const void *src, size_t n);
 
 void *memchr(const void *s, int c, size_t n);
 void *memrchr(const void *s, int c, size_t n);
