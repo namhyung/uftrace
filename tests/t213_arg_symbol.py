d91a3c8d35ac7df08a6526c5a4b6df4fd47b55a5
Namhyung Kim <namhyung@gmail.com>
2018-12-24 20:11:20 +0900

    test: Add a test case for data symbol display
      $ uftrace -a tests/t-data
      # DURATION     TID     FUNCTION
                  [ 16187] | main() {
         0.967 us [ 16187] |   foo(&static_var, 1) = 2;
         0.331 us [ 16187] |   foo(&global_var, 2) = 3;
         0.143 us [ 16187] |   foo(&weak_var, 3) = 4;
         0.166 us [ 16187] |   filecmp(&_IO_2_1_stdout_, &_IO_2_1_stderr_) = 0;
         3.189 us [ 16187] | } = 0; /* main */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t213_arg_symbol.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'data', result="""
+# DURATION     TID     FUNCTION
+            [ 16187] | main() {
+   0.967 us [ 16187] |   foo(&static_var, 1);
+   0.331 us [ 16187] |   foo(&global_var, 2);
+   0.143 us [ 16187] |   foo(&weak_var, 3);
+   0.166 us [ 16187] |   filecmp(&_IO_2_1_stdout_, &_IO_2_1_stderr_);
+   3.189 us [ 16187] | } /* main */
+""")
+
+    def build(self, name, cflags='', ldflags=''):
+        # cygprof doesn't support arguments now
+        if cflags.find('-finstrument-functions') >= 0:
+            return TestBase.TEST_SKIP
+
+        return TestBase.build(self, name, cflags, ldflags)
+
+    def runcmd(self):
+        uftrace = TestBase.uftrace_cmd
+        options = '-A foo@arg1,arg2 -A filecmp@arg1,arg2'
+        program = 't-' + self.name
+
+        return '%s %s %s' % (uftrace, options, program)
