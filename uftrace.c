257dd881090201aacce18059e00e1fd5178d0ba2
Honggyu Kim <honggyu.kp@gmail.com>
2019-01-16 12:04:00 +0900

    uftrace: Add mcount and trigger functions to uftrace.h
    This patch adds some convenient features as follows:
    - mcount() declaration for explicit function tracing without -pg
    - uftrace_trace_on() to enable "trace_on" trigger
    - uftrace_trace_off() to enable "trace_off" trigger
    - uftrace_trace_finish() to enable "finish" trigger
    - class uftrace_trace_block to block-scope tracing on/off
    
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- uftrace.c
+++ uftrace.c
@@ -988,6 +988,12 @@ int main(int argc, char *argv[])
 		.sort_column	= 2,
 		.event_skip_out = true,
 		.patt_type      = PATT_REGEX,
+		.trigger        = xstrdup(
+				  "uftrace_trace_on@trace_on,color=green;"
+				  "uftrace_trace_off@trace_off,color=green;"
+				  "uftrace_trace_finish@finish,color=green;"
+				  "uftrace_trace_block::uftrace_trace_block@trace_on,color=green;"
+				  "uftrace_trace_block::~uftrace_trace_block@trace_off,color=green"),
 	};
 	struct argp argp = {
 		.options = uftrace_options,
