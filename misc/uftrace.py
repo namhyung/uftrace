44c6427286d6130a72d71038a5eac8cc65e4407e
Honggyu Kim <honggyu.kp@gmail.com>
2019-02-16 18:55:02 +0900

    python: Fix argument handling of misc/uftrace.py
    misc/uftrace.py indirectly executes the given python script, but it
    shouldn't be passed as the first argument in sys.argv.
    
    This patch also changes execfile() to exec() to make it work in both
    python2 and python3.
    
    In "What’s New In Python 3.0":
    "Removed execfile(). Instead of execfile(fn) use exec(open(fn).read())."
    
    Link: http://docs.python.org/3.3/whatsnew/3.0.html?highlight=execfile#builtins
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- misc/uftrace.py
+++ misc/uftrace.py
@@ -1,6 +1,17 @@
 import sys
 import trace_python
 
+sys.argv = sys.argv[1:]
+
+progname = sys.argv[0]
+
+globs = {
+    '__file__': progname,
+    '__name__': '__main__',
+    '__package__': None,
+    '__cached__': None,
+}
+
 sys.settrace(trace_python.trace)
 
-execfile(sys.argv[1])
+exec(open(progname).read(), globs, globs)
