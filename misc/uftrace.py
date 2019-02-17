c9f4e802e239e49c0251d016c0b338ceb69b4916
Honggyu Kim <honggyu.kp@gmail.com>
2019-02-17 13:38:28 +0900

    python: Properly use trace-python?.so based on its version
    misc/uftrace.py is a middle layer before calling the target python
    script, so it can check the python version before tracing.
    
    Based on the python version, it can decide whether to import
    "trace_python2.so" or "trace_python3.so".
    
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- misc/uftrace.py
+++ misc/uftrace.py
@@ -1,5 +1,11 @@
 import sys
-import trace_python2
+
+if sys.version_info[0] < 3:
+    import trace_python2
+    sys.settrace(trace_python2.trace)
+else:
+    import trace_python3
+    sys.settrace(trace_python3.trace)
 
 sys.argv = sys.argv[1:]
 
@@ -11,7 +17,5 @@ globs = {
     '__package__': None,
     '__cached__': None,
 }
-
-sys.settrace(trace_python2.trace)
 
 exec(open(progname).read(), globs, globs)
