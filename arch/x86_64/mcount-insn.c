54568fab258ab43aeeadecaeee51fce4e8dad073
Hanbum Park <kese111@gmail.com>
2019-09-05 23:18:28 -0700

    dynamic: support position indepdent code if opcode is 'LEA' only
    support position independent code, it is need to manipulate instruction,
    therefore added some code to do this. specially, for supprot PIC code,
    it is need to add additional instruction and calculate actually affected
    address. this commit contain how to support PIC code very limited when
    instruction was "lea".
    
    for details:
    =====================================
    lea rcx, qword ptr [rip + 0x8f3f85]
    =====================================
    
    change to two instruction like below,
    ======================================
    mov rcx, [instruction located address]
    lea rcx, qword ptr [rcx + 0x8f3f85]
    ======================================
    
    Signed-off-by: Hanbum Park <kese111@gmail.com>

--- arch/x86_64/mcount-insn.c
+++ arch/x86_64/mcount-insn.c
@@ -63,6 +63,248 @@ void print_instrument_fail_msg(int reason)
 	}
 }
 
+/*
+ *  handle PIC code.
+ *  for currently, this function targeted specific type of instruction.
+ *
+ *  this function manipulate the instruction like below,
+ *  => lea rcx, qword ptr [rip + 0x8f3f85]
+ *  to this.
+ *  => mov rcx, [address where instruction located actually]
+ *  => lea rcx, qword ptr [rcx + 0x8f3f85]
+ */
+static int handle_pic(cs_insn *insn, uint8_t insns[])
+{
+	cs_x86 *x86 = &insn->detail->x86;
+
+	/*
+	 * array for mov instruction.
+	 * ex) mov rbx, 0x555556d35690
+	 */
+	uint8_t mov_insns[] = {
+		0x48, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	};
+	int mov_rex_offset = 0;
+	int mov_opnd_offset = 1;
+	int mov_imm64_offset = 2;
+
+	/*
+	 * array for lea instruction.
+	 * ex) lea rbx, [rbx+0x8f3f85]
+	 */
+	uint8_t lea_insns[] = {
+		0x48, 0x8d, 0x00, 0x00, 0x00, 0x00, 0x00
+	};
+	int lea_rex_offset = 0;
+	int lea_insn_offset = 1;
+	int lea_opnd_offset = 2;
+	int lea_disp_offset = 3;
+
+	// REX.48 operands
+	uint8_t mov_opnd_48[] = {
+		//rax,	rbx,	rcx,	rdx,	rdi,	rsi,	rbp,	rsp
+		0xb8,	0xbb,	0xb9,	0xba,	0xbf,	0xbe,	0xbd,	0xbc
+	};
+
+	// REX.49 operands
+	uint8_t mov_opnd_49[] = {
+		//r8,	r9,	r10,	r11,	r12,	r13,	r14,	r15
+		0xb8,	0xb9,	0xba,	0xbb,	0xbc,	0xbd,	0xbe,	0xbf
+	};
+
+	// REX.48 operands,
+	uint8_t lea_opnd_48[] = {
+		//rax,	rbx,	rcx,	rdx,	rdi,	rsi,	rbp,	rsp
+		0x80,	0x9b,	0x89,	0x92,	0xbf,	0xb6,	0xad,	0xa4
+	};
+
+	// REX.4D operands,
+	uint8_t lea_opnd_4D[] = {
+		//r8,	r9,	r10,	r11,	r12,	r13,	r14,	r15
+		0x80,	0x89,	0x92,	0x9b,	0xa4,	0xad,	0xb6,	0xbf
+	};
+
+	// for currently, support LEA instruction only.
+	if (strcmp(insn->mnemonic, "lea") != 0)
+		goto out;
+
+	// according to intel manual, lea instruction takes 2 operand only.
+	cs_x86_op *opnd1 = &(x86->operands[0]);
+	cs_x86_op *opnd2 = &(x86->operands[1]);
+
+	if (opnd2->type != X86_OP_MEM || opnd2->mem.base != X86_REG_RIP)
+		goto out;
+
+	// handling the SIB not supported yet.
+	if (opnd2->mem.scale > 1 || opnd2->mem.disp == 0)
+		goto out;
+
+	// get the register from opnd1 to load rip address
+	if (X86_REG_RAX <= opnd1->reg && opnd1->reg <= X86_REG_RSP) {
+		mov_insns[mov_rex_offset] = 0x48;
+		lea_insns[lea_rex_offset] = 0x48;
+	}
+	else if (X86_REG_R8 <= opnd1->reg && opnd1->reg <= X86_REG_R15) {
+		mov_insns[mov_rex_offset] = 0x49;
+		lea_insns[lea_rex_offset] = 0x4D;
+	}
+	else
+		goto out;
+
+	switch(opnd1->reg) {
+	// REX.48
+	case X86_REG_RAX:
+		mov_insns[mov_opnd_offset] = mov_opnd_48[0];
+		break;
+	case X86_REG_RBX:
+		mov_insns[mov_opnd_offset] = mov_opnd_48[1];
+		break;
+	case X86_REG_RCX:
+		mov_insns[mov_opnd_offset] = mov_opnd_48[2];
+		break;
+	case X86_REG_RDX:
+		mov_insns[mov_opnd_offset] = mov_opnd_48[3];
+		break;
+	case X86_REG_RDI:
+		mov_insns[mov_opnd_offset] = mov_opnd_48[4];
+		break;
+	case X86_REG_RSI:
+		mov_insns[mov_opnd_offset] = mov_opnd_48[5];
+		break;
+	case X86_REG_RBP:
+		mov_insns[mov_opnd_offset] = mov_opnd_48[6];
+		break;
+	case X86_REG_RSP:
+		mov_insns[mov_opnd_offset] = mov_opnd_48[7];
+		break;
+
+	// REX.49
+	case X86_REG_R8:
+		mov_insns[mov_opnd_offset] = mov_opnd_49[0];
+		break;
+	case X86_REG_R9:
+		mov_insns[mov_opnd_offset] = mov_opnd_49[1];
+		break;
+	case X86_REG_R10:
+		mov_insns[mov_opnd_offset] = mov_opnd_49[2];
+		break;
+	case X86_REG_R11:
+		mov_insns[mov_opnd_offset] = mov_opnd_49[3];
+		break;
+	case X86_REG_R12:
+		mov_insns[mov_opnd_offset] = mov_opnd_49[4];
+		break;
+	case X86_REG_R13:
+		mov_insns[mov_opnd_offset] = mov_opnd_49[5];
+		break;
+	case X86_REG_R14:
+		mov_insns[mov_opnd_offset] = mov_opnd_49[6];
+		break;
+	case X86_REG_R15:
+		mov_insns[mov_opnd_offset] = mov_opnd_49[7];
+		break;
+	default:
+		goto out;
+	}
+
+	uint64_t PIC_base = insn->address;
+	PIC_base += insn->size;
+	*(uint64_t *)(mov_insns + mov_imm64_offset) = PIC_base;
+
+	switch (opnd1->reg) {
+	//rax,	rbx,	rcx,	rdx,	rdi,	rsi,	rbp,	rsp
+	case X86_REG_RAX:
+		lea_insns[lea_opnd_offset] = lea_opnd_48[0];
+		break;
+	case X86_REG_RBX:
+		lea_insns[lea_opnd_offset] = lea_opnd_48[1];
+		break;
+	case X86_REG_RCX:
+		lea_insns[lea_opnd_offset] = lea_opnd_48[2];
+		break;
+	case X86_REG_RDX:
+		lea_insns[lea_opnd_offset] = lea_opnd_48[3];
+		break;
+	case X86_REG_RDI:
+		lea_insns[lea_opnd_offset] = lea_opnd_48[4];
+		break;
+	case X86_REG_RSI:
+		lea_insns[lea_opnd_offset] = lea_opnd_48[5];
+		break;
+	case X86_REG_RBP:
+		lea_insns[lea_opnd_offset] = lea_opnd_48[6];
+		break;
+	case X86_REG_RSP:
+		/*
+		 * could not handling this case yet because
+		 * need to add SIB for handling this case.
+		 */
+		goto out;
+
+	// REX.4D
+	case X86_REG_R8:
+		lea_insns[lea_opnd_offset] = lea_opnd_4D[0];
+		break;
+	case X86_REG_R9:
+		lea_insns[lea_opnd_offset] = lea_opnd_4D[1];
+		break;
+	case X86_REG_R10:
+		lea_insns[lea_opnd_offset] = lea_opnd_4D[2];
+		break;
+	case X86_REG_R11:
+		lea_insns[lea_opnd_offset] = lea_opnd_4D[3];
+		break;
+	case X86_REG_R12:
+		/*
+		 * could not handling this case yet because
+		 * need to add SIB for handling this case.
+		 */
+		goto out;
+	case X86_REG_R13:
+		lea_insns[lea_opnd_offset] = lea_opnd_4D[5];
+		break;
+	case X86_REG_R14:
+		lea_insns[lea_opnd_offset] = lea_opnd_4D[6];
+		break;
+	case X86_REG_R15:
+		lea_insns[lea_opnd_offset] = lea_opnd_4D[7];
+		break;
+	default:
+		goto out;
+	}
+
+	// according to rule of modR/M in intel, disp has 32bit size maximum.
+	int32_t disp = (int32_t)opnd2->mem.disp;
+	*(uint32_t *)(lea_insns + lea_disp_offset) = disp;
+
+	memcpy(insns, (void *)mov_insns, sizeof(mov_insns));
+	memcpy(insns + sizeof(mov_insns), (void *)lea_insns, sizeof(lea_insns));
+
+	return sizeof(mov_insns) + sizeof(lea_insns);
+
+out:
+	return -1;
+}
+
+static int manipulate_insns(cs_insn *insn, uint8_t insns[], int* fail_reason)
+{
+	int res = -1;
+
+	pr_dbg3("Try to instrument if instruction could be manipulate possibly.\n");
+	switch(*fail_reason) {
+		case INSTRUMENT_FAIL_PICCODE:
+			res = handle_pic(insn, insns);
+			if (res > 0) {
+				*fail_reason ^= INSTRUMENT_FAIL_PICCODE;
+			}
+			break;
+		default:
+			break;
+	}
+
+	return res;
+}
+
 static int copy_insn_bytes(cs_insn *insn, uint8_t insns[])
 {
 	int res = insn->size;
@@ -124,7 +366,6 @@ static int check_instrumentable(struct mcount_disasm_engine *disasm,
 				status |= INSTRUMENT_FAIL_RELCALL;
 			else if (check_branch == OP_GROUP_JMP)
 				status |= INSTRUMENT_FAIL_RELJMP;
-
 
 			goto out;
 
@@ -196,7 +437,6 @@ static bool check_unsupported(struct mcount_disasm_engine *disasm, cs_insn *insn
 		case X86_OP_MEM:
 			/* indirect jumps are not allowed */
 			return false;
-			break;
 		case X86_OP_REG:
 			return false;
 		default:
@@ -225,13 +465,13 @@ disasm_check_insns(struct mcount_disasm_engine *disasm,
 	struct mcount_instrument_info *info = xzalloc(sizeof(*info));
 
 	info->addr = sym->addr + mdi->map->start;
-	info->size = INSTRUMENT_FAILED;
 	info->func_size = sym->size;
 
 	badsym = find_bad_jump(mdi, insn_check.addr);
 	if (badsym != NULL) {
 		list_del(&badsym->list);
 		free(badsym);
+		info->size = INSTRUMENT_FAILED;
 		return info;
 	}
 
@@ -250,9 +490,13 @@ disasm_check_insns(struct mcount_disasm_engine *disasm,
 	}
 
 	for (i = 0; i < count; i++) {
-		insns_size = 0;
 		memset(insns_byte, 0, MAX_INSNS_BYTES);
 		status = check_instrumentable(disasm, &insn[i]);
+
+		if (status > 0)
+			insns_size = manipulate_insns(&insn[i], insns_byte, &status);
+		else
+			insns_size = copy_insn_bytes(&insn[i], insns_byte);
 
 		if (status > 0) {
 			info->size = INSTRUMENT_FAILED;
@@ -261,10 +505,9 @@ disasm_check_insns(struct mcount_disasm_engine *disasm,
 			goto out;
 		}
 
-		insns_size = copy_insn_bytes(&insn[i], insns_byte);
 		info->insns = xrealloc(info->insns,
 				       info->insns_size + insns_size);
-		memcpy(info->insns + info->insns_size, (void *)insns_byte, insns_size);
+		memcpy(info->insns + info->insns_size, insns_byte, insns_size);
 		info->insns_size += insns_size;
 		insn_check.copy_size += insn[i].size;
 		info->size += insn[i].size;
