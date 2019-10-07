6aa85142af02f12461f23fa32a94e238fe4c7bef
Honggyu Kim <honggyu.kp@gmail.com>
2019-10-08 00:22:21 +0900

    utils: Load python symbols as another module
    This patch is to cope with some changes from the following commit:
    
      d8eb70b symbol: Use module symbol table in find_symtabs()
    
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- utils/symbol.c
+++ utils/symbol.c
@@ -792,12 +792,11 @@ void load_python_symtab(struct symtabs *symtabs)
 	memcpy(map->prot, "rwxp", 4);
 	strcpy(map->libname, PYTHON_SYMTAB_NAME);
 
-	load_module_symbol_file(&map->symtab, symfile, 0);
-	setup_debug_info(symfile, &map->dinfo, 0, false);
-
 	/* add new map to symtabs */
 	map->next = symtabs->maps;
 	symtabs->maps = map;
+
+	load_module_symtab(symtabs, map->libname);
 
 	free(symfile);
 }
