46ce1b9b86254e982a950ff776b03d1bc1070e70
Hanbum Park <kese111@gmail.com>
2019-10-08 19:01:08 +0900

    dynamic: change module name source from memory map to filename
    typically, executable binary has lowest memory map and first module name
    in process memory map. uftrace's one logic also based on this. however
    WSL2 does not guarantee this. so, one logic in uftrace based on
    assumptions mentioned above not work properly. this PR fix this.
    
    fixed: #852
    
    Signed-off-by: Hanbum Park <kese111@gmail.com>

--- libmcount/dynamic.c
+++ libmcount/dynamic.c
@@ -292,7 +292,7 @@ static int do_dynamic_update(struct symtabs *symtabs, char *patch_funcs,
 	if (patch_funcs == NULL)
 		return 0;
 
-	def_mod = basename(symtabs->maps->libname);
+	def_mod = basename(symtabs->filename);
 	strv_split(&funcs, patch_funcs, ";");
 
 	strv_for_each(&funcs, name, j) {
