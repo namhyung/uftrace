bfc7011ec8d84bd191063b0ca0ad9976adc52e46
Byeonggon Lee <gonny952@gmail.com>
2019-10-18 16:11:58 +0900

    utils: Support portable versionsort
    In some non-libc envirnment such as bionic,
    there is no versionsort and related functions.
    
    Signed-off-by: Byeonggon Lee <gonny952@gmail.com>

---
+++ check-deps/__have_strcoll.c
@@ -0,0 +1,7 @@
+#include <string.h>
+
+int main(void)
+{
+	int diff = strcoll("A", "B");
+	return 0;
+}
---
+++ check-deps/__have_strverscmp.c
@@ -0,0 +1,8 @@
+#define _GNU_SOURCE
+#include <string.h>
+
+int main(void)
+{
+	strverscmp("A2", "B1");
+	return 0;
+}
