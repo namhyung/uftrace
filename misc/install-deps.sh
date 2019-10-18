namhyung/uftrace

commit ffb70d31186b48c3c44361113b6c482d784cb64f

misc: Add Manjaro, Alpine support to install-deps.sh
Signed-off-by: Byeonggon Lee <gonny952@gmail.com>

misc/install-deps.sh
@@ -22,9 +22,12 @@ case $distro in
         rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
         yum install $OPT pandoc elfutils-devel python2-devel ncurses-devel pkgconfig
         yum install $OPT capstone-devel ;;
-    "arch")
+    "arch" | "manjaro")
 	pacman $OPT -S pandoc libelf python2 ncurses pkgconf
 	pacman $OPT -S capstone ;;
+    "alpine")
+        apk $OPT add elfutils-dev python2-dev ncurses-dev pkgconf
+        apk $OPT add capstone-dev
     *) # we can add more install command for each distros.
         echo "\"$distro\" is not supported distro, so please install packages manually." ;;
 esac

commit 772bfd693dbb954ad62b01f79408548347a127c9

misc: Fix missing ;; in install-deps.sh
The commit ffb70d3 introduced the following problem.

  ./misc/install-deps.sh: Syntax error: ")" unexpected (expecting ";;")

Signed-off-by: Sungho Yoon <lacrimak3@gmail.com>

misc/install-deps.sh
@@ -27,7 +27,7 @@ case $distro in
 	pacman $OPT -S capstone ;;
     "alpine")
         apk $OPT add elfutils-dev python2-dev ncurses-dev pkgconf
-        apk $OPT add capstone-dev
+        apk $OPT add capstone-dev ;;
     *) # we can add more install command for each distros.
         echo "\"$distro\" is not supported distro, so please install packages manually." ;;
 esac