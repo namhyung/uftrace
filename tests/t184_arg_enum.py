aac41226d5c73392d6708c9ee85b470dcfe29bd6
Honggyu Kim <honggyu.kp@gmail.com>
2020-09-13 15:09:19 +0100

    tests: Fix the test 184 arg_enum
    The test 184 arg_enum fails because the file descriptor number is
    different from the expected number, but this patch does post processing
    to make it just 'FD' for better comparison.
    
      t184_arg_enum: diff result of -pg -O0
      --- expect      2020-09-13 15:08:24.164255538 +0100
      +++ result      2020-09-13 15:08:24.164255538 +0100
      @@ -2,7 +2,7 @@
          foo() {
      -     open("/dev/zero", O_RDONLY) = 4;
      -     mmap(0, 4096, PROT_READ, MAP_ANON|MAP_PRIVATE, 4, 0) = 0xADDR;
      +     open("/dev/zero", O_RDONLY) = 6;
      +     mmap(0, 4096, PROT_READ, MAP_ANON|MAP_PRIVATE, 6, 0) = 0xADDR;
            mprotect(0xADDR, 4096, PROT_NONE) = 0;
            munmap(0xADDR, 4096) = 0;
      -     close(4) = 0;
      +     close(6) = 0;
          } /* foo */
    
      184 arg_enum            : NG
    
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- tests/t184_arg_enum.py
+++ tests/t184_arg_enum.py
@@ -9,11 +9,11 @@ class TestCase(TestBase):
 # DURATION    TID     FUNCTION
             [14885] | main() {
             [14885] |   foo() {
-   9.397 us [14885] |     open("/dev/zero", O_RDONLY) = 5;
-   3.802 us [14885] |     mmap(0, 4096, PROT_READ, MAP_ANON|MAP_PRIVATE, 5, 0) = 0xADDR;
+   9.397 us [14885] |     open("/dev/zero", O_RDONLY) = FD;
+   3.802 us [14885] |     mmap(0, 4096, PROT_READ, MAP_ANON|MAP_PRIVATE, FD, 0) = 0xADDR;
    4.119 us [14885] |     mprotect(0xADDR, 4096, PROT_NONE) = 0;
    6.183 us [14885] |     munmap(0xADDR, 4096) = 0;
-   3.120 us [14885] |     close(5) = 0;
+   3.120 us [14885] |     close(FD) = 0;
   36.529 us [14885] |   } /* foo */
   37.849 us [14885] | } /* main */
 """)
@@ -28,7 +28,15 @@ class TestCase(TestBase):
             if ln.strip() == '' or ln.startswith('#'):
                 continue
             line = ln.split('|', 1)[-1]
+
+            # address might be different, so replace it as 0xADDR for comparison.
             func = re.sub(r'0x[0-9a-f]+', '0xADDR', line)
+
+            # fd number might be different, so replace it as FD for comparison.
+            func = re.sub(r'O_RDONLY\) = [0-9]+', 'O_RDONLY) = FD', func)
+            func = re.sub(r'MAP_PRIVATE, [0-9]+, 0', 'MAP_PRIVATE, FD, 0', func)
+            func = re.sub(r'close\([0-9]+\)', 'close(FD)', func)
+
             result.append(func)
 
         return '\n'.join(result)
