5498d030bd89ace20757629b6f34657a0df85c09
Namhyung Kim <namhyung@gmail.com>
2020-10-04 15:42:41 +0900

    misc: Add -v option to dbginfo
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- misc/dbginfo.c
+++ misc/dbginfo.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 	enum uftrace_pattern_type ptype = PATT_REGEX;
 	int opt;
 
-	while ((opt = getopt(argc, argv, "aA:R:")) != -1) {
+	while ((opt = getopt(argc, argv, "aA:R:v")) != -1) {
 		switch (opt) {
 		case 'a':
 			auto_args = true;
@@ -67,6 +67,10 @@ int main(int argc, char *argv[])
 			break;
 		case 'R':
 			retspec = optarg;
+			break;
+		case 'v':
+			debug++;
+			dbg_domain[DBG_DWARF]++;
 			break;
 		default:
 			printf("dbginfo: unknown option: %c\n", opt);
@@ -79,6 +83,9 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	filename = argv[optind];
+
+	logfp = stderr;
+	outfp = stdout;
 
 	map = xzalloc(sizeof(*map) + strlen(filename) + 1);
 	strcpy(map->libname, filename);
