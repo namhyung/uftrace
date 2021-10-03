2794c6fc761842247cf8587ae108cae755d5a740
Honggyu Kim <honggyu.kp@gmail.com>
2021-10-03 20:10:10 +0100

    dynamic: Support -fpatchable-function-entry=2 tracing for aarch64
    This patch makes uftrace use -fpatchable-function-entry=N in aarch64.
    The N should be 2(or greater) because it requires 2 instructions to be
    patched at the entry of functions.
    
    The example usage is as follows.
    
      $ gcc -fpatchable-function-entry=2 tests/s-abc.c
    
      $ uftrace -P. --debug-domain dynamic:3 a.out
      dynamic: dynamic patch type: a.out: 3 (fentry-nop)
      dynamic: update function 'a' dynamically to call __fentry__
      dynamic: update function 'b' dynamically to call __fentry__
      dynamic: update function 'c' dynamically to call __fentry__
      dynamic: update function 'main' dynamically to call __fentry__
      dynamic: patched all (4) functions in 'a.out'
      # DURATION     TID     FUNCTION
                  [ 30535] | main() {
                  [ 30535] |   a() {
                  [ 30535] |     b() {
                  [ 30535] |       c() {
         0.937 us [ 30535] |         getpid();
         4.143 us [ 30535] |       } /* c */
         4.618 us [ 30535] |     } /* b */
         5.078 us [ 30535] |   } /* a */
         5.769 us [ 30535] | } /* main */
    
    Since aarch64 doesn't have __fentry__ hook implementation, this patch
    also provides it, but it's modified based on __dentry__ but without
    looking up the original code.
    
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- arch/aarch64/mcount-dynamic.c
+++ arch/aarch64/mcount-dynamic.c
@@ -38,6 +38,7 @@ static void save_orig_code(struct mcount_disasm_info *info)
 int mcount_setup_trampoline(struct mcount_dynamic_info *mdi)
 {
 	uintptr_t dentry_addr = (uintptr_t)(void *)&__dentry__;
+	uintptr_t fentry_addr = (uintptr_t)(void *)&__fentry__;
 	/*
 	 * trampoline assumes {x29,x30} was pushed but x29 was not updated.
 	 * make sure stack is 8-byte aligned.
@@ -49,6 +50,11 @@ int mcount_setup_trampoline(struct mcount_dynamic_info *mdi)
 		dentry_addr,
 		dentry_addr >> 32,
 	};
+
+	if (mdi->type == DYNAMIC_PATCHABLE) {
+		trampoline[3] = fentry_addr;
+		trampoline[4] = fentry_addr >> 32;
+	}
 
 	/* find unused 16-byte at the end of the code segment */
 	mdi->trampoline  = ALIGN(mdi->text_addr + mdi->text_size, PAGE_SIZE);
@@ -76,52 +82,162 @@ int mcount_setup_trampoline(struct mcount_dynamic_info *mdi)
 	return 0;
 }
 
+static void read_patchable_loc(struct mcount_dynamic_info *mdi,
+			    struct uftrace_elf_data *elf,
+			    struct uftrace_elf_iter *iter,
+			    unsigned long offset)
+{
+	typeof(iter->shdr) *shdr = &iter->shdr;
+
+	mdi->nr_patch_target = shdr->sh_size / sizeof(long);
+	mdi->patch_target = xmalloc(shdr->sh_size);
+
+	elf_get_secdata(elf, iter);
+	elf_read_secdata(elf, iter, 0, mdi->patch_target, shdr->sh_size);
+
+	/* symbol has relative address, fix it to match each other */
+	if (elf->ehdr.e_type == ET_EXEC) {
+		unsigned long *patchable_loc = mdi->patch_target;
+		unsigned i;
+
+		for (i = 0; i < mdi->nr_patch_target; i++) {
+			patchable_loc[i] -= offset;
+		}
+	}
+}
+
+void mcount_arch_find_module(struct mcount_dynamic_info *mdi,
+			     struct symtab *symtab)
+{
+	struct uftrace_elf_data elf;
+	struct uftrace_elf_iter iter;
+
+	mdi->type = DYNAMIC_NONE;
+
+	if (elf_init(mdi->map->libname, &elf) < 0)
+		goto out;
+
+	elf_for_each_shdr(&elf, &iter) {
+		char *shstr = elf_get_name(&elf, &iter, iter.shdr.sh_name);
+
+		if (!strcmp(shstr, PATCHABLE_SECT)) {
+			mdi->type = DYNAMIC_PATCHABLE;
+			read_patchable_loc(mdi, &elf, &iter, mdi->base_addr);
+			goto out;
+		}
+	}
+
+	switch (check_trace_functions(mdi->map->libname)) {
+	case TRACE_MCOUNT:
+		mdi->type = DYNAMIC_PG;
+		break;
+	default:
+		break;
+	}
+
+out:
+	pr_dbg("dynamic patch type: %s: %d (%s)\n", basename(mdi->map->libname),
+	       mdi->type, mdi_type_names[mdi->type]);
+
+	elf_finish(&elf);
+}
+
 static unsigned long get_target_addr(struct mcount_dynamic_info *mdi,
 				     unsigned long addr)
 {
+	/* encode the target address of the trampoline */
 	return (mdi->trampoline - addr - 4) >> 2;
 }
 
-int mcount_patch_func(struct mcount_dynamic_info *mdi, struct sym *sym,
-		      struct mcount_disasm_engine *disasm, unsigned min_size)
+static int patch_code(struct mcount_dynamic_info *mdi, struct sym *sym)
 {
 	uint32_t push = 0xa9bf7bfd;  /* STP  x29, x30, [sp, #-0x10]! */
 	uint32_t call;
-	struct mcount_disasm_info info = {
-		.sym = sym,
-		.addr = sym->addr + mdi->map->start,
-	};
-	void *insn = (void *)info.addr;
+	void *insn = (void *)sym->addr + mdi->map->start;
 
-	if (min_size < CODE_SIZE)
-		min_size = CODE_SIZE;
-	if (sym->size <= min_size)
-		return INSTRUMENT_SKIPPED;
-
-	if (disasm_check_insns(disasm, mdi, &info) < 0)
-		return INSTRUMENT_FAILED;
-
-	save_orig_code(&info);
-
-	call = get_target_addr(mdi, info.addr);
+	call = get_target_addr(mdi, (unsigned long)insn);
 
 	if ((call & 0xfc000000) != 0)
 		return INSTRUMENT_FAILED;
-
-	pr_dbg2("patch normal func: %s (patch size: %d)\n",
-		sym->name, info.orig_size);
 
 	/* make a "BL" insn with 26-bit offset */
 	call |= 0x94000000;
 
 	/* hopefully we're not patching 'memcpy' itself */
 	memcpy(insn, &push, sizeof(push));
-	memcpy(insn+4, &call, sizeof(call));
+	memcpy(insn + 4, &call, sizeof(call));
 
 	/* flush icache so that cpu can execute the new code */
 	__builtin___clear_cache(insn, insn + CODE_SIZE);
 
 	return INSTRUMENT_SUCCESS;
+}
+
+static int patch_patchable_func(struct mcount_dynamic_info *mdi, struct sym *sym)
+{
+	unsigned int patchable_nop_patt[] = { 0xd503201f, 0xd503201f };
+	void *insn = (void *)sym->addr + mdi->map->start;
+
+	/* only support calls to 2 NOPs at the beginning */
+	if (memcmp(insn, patchable_nop_patt, sizeof(patchable_nop_patt))) {
+		pr_dbg("skip non-applicable functions: %s\n", sym->name);
+		return INSTRUMENT_FAILED;
+	}
+
+	if (patch_code(mdi, sym) < 0)
+		return INSTRUMENT_FAILED;
+
+	pr_dbg3("update %p for '%s' function dynamically to call __fentry__\n",
+		insn, sym->name);
+
+	return INSTRUMENT_SUCCESS;
+}
+
+static int patch_normal_func(struct mcount_dynamic_info *mdi, struct sym *sym,
+			     struct mcount_disasm_engine *disasm)
+{
+	struct mcount_disasm_info info = {
+		.sym = sym,
+		.addr = sym->addr + mdi->map->start,
+	};
+
+	if (disasm_check_insns(disasm, mdi, &info) < 0)
+		return INSTRUMENT_FAILED;
+
+	save_orig_code(&info);
+
+	if (patch_code(mdi, sym) < 0)
+		return INSTRUMENT_FAILED;
+
+	pr_dbg3("force patch normal func: %s (patch size: %d)\n",
+		sym->name, info.orig_size);
+
+	return INSTRUMENT_SUCCESS;
+}
+
+int mcount_patch_func(struct mcount_dynamic_info *mdi, struct sym *sym,
+		      struct mcount_disasm_engine *disasm, unsigned min_size)
+{
+	int result = INSTRUMENT_SKIPPED;
+
+	if (min_size < CODE_SIZE)
+		min_size = CODE_SIZE;
+	if (sym->size <= min_size)
+		return INSTRUMENT_SKIPPED;
+
+	switch (mdi->type) {
+	case DYNAMIC_PATCHABLE:
+		result = patch_patchable_func(mdi, sym);
+		break;
+
+	case DYNAMIC_NONE:
+		result = patch_normal_func(mdi, sym, disasm);
+		break;
+
+	default:
+		break;
+	}
+	return result;
 }
 
 static void revert_normal_func(struct mcount_dynamic_info *mdi, struct sym *sym,
