e10a596eba1688b2f0a64c988a80dcbc0e2e435b
Honggyu Kim <honggyu.kp@gmail.com>
2021-10-03 20:10:10 +0100

    dynamic: Support -fpatchable-function-entry=5 tracing for x86_64
    Recnet gcc and clang provides a way to have NOP sleds at the beginning
    of functions with new option '-fpatchable-function-entry=N[,M]'.
    
    The explanation in gcc[1] is as follows.
    
      -fpatchable-function-entry=N[,M]
        Generate N NOPs right at the beginning of each function, with the
        function entry point before the Mth NOP. If M is omitted, it
        defaults to 0 so the function entry points to the address just at
        the first NOP. The NOP instructions reserve extra space which can be
        used to patch in any desired instrumentation at run time, provided
        that the code segment is writable. The amount of space is
        controllable indirectly via the number of NOPs; the NOP instruction
        used corresponds to the instruction emitted by the internal GCC
        back-end interface gen_nop. This behavior is target-specific and may
        also depend on the architecture variant and/or other compilation
        options.
    
        For run-time identification, the starting addresses of these areas,
        which correspond to their respective function entries minus M, are
        additionally collected in the __patchable_function_entries section
        of the resulting binary.
    
        Note that the value of __attribute__ ((patchable_function_entry (N,M)))
        takes precedence over command-line option -fpatchable-function-entry=N,M.
        This can be used to increase the area size or to remove it completely
        on a single function. If N=0, no pad location is recorded.
    
        The NOP instructions are inserted at—and maybe before, depending on
        M—the function entry address, even before the prologue.
    
        The maximum value of N and M is 65535.
    
    This patch makes uftrace use -fpatchable-function-entry=N in x86_64.
    The N should be 5(or greater) because The call instruction requires 5
    bytes.  The example usage is as follows.
    
      $ gcc -fpatchable-function-entry=5 tests/s-abc.c
    
      $ uftrace -P. --debug-domain dynamic:3 a.out
      dynamic: dynamic patch type: a.out: 3 (fentry-nop)
      dynamic: update function 'a' dynamically to call __fentry__
      dynamic: update function 'b' dynamically to call __fentry__
      dynamic: update function 'c' dynamically to call __fentry__
      dynamic: update function 'main' dynamically to call __fentry__
      dynamic: patched all (4) functions in 'a.out'
      # DURATION     TID     FUNCTION
                  [ 30535] | main() {
                  [ 30535] |   a() {
                  [ 30535] |     b() {
                  [ 30535] |       c() {
         0.937 us [ 30535] |         getpid();
         4.143 us [ 30535] |       } /* c */
         4.618 us [ 30535] |     } /* b */
         5.078 us [ 30535] |   } /* a */
         5.769 us [ 30535] | } /* main */
    
    Note that this feature reuses __fentry__ hook so it doesn't require
    extra trampoline to handle original instructions as it does in full
    dynamic tracing.
    
    Closes: #1238
    
    [1] https://gcc.gnu.org/onlinedocs/gcc/Instrumentation-Options.html#index-fpatchable-function-entry
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- arch/x86_64/mcount-dynamic.c
+++ arch/x86_64/mcount-dynamic.c
@@ -62,7 +62,7 @@ int mcount_setup_trampoline(struct mcount_dynamic_info *mdi)
 		memcpy((void *)mdi->trampoline + 16 + sizeof(trampoline),
 		       &xray_exit_addr, sizeof(xray_exit_addr));
 	}
-	else if (mdi->type == DYNAMIC_FENTRY_NOP) {
+	else if (mdi->type == DYNAMIC_FENTRY_NOP || mdi->type == DYNAMIC_PATCHABLE) {
 		/* jmpq  *0x1(%rip)     # <fentry_addr> */
 		memcpy((void *)mdi->trampoline, trampoline, sizeof(trampoline));
 		memcpy((void *)mdi->trampoline + sizeof(trampoline),
@@ -142,6 +142,30 @@ static void read_mcount_loc(struct mcount_dynamic_info *mdi,
 	}
 }
 
+static void read_patchable_loc(struct mcount_dynamic_info *mdi,
+			    struct uftrace_elf_data *elf,
+			    struct uftrace_elf_iter *iter,
+			    unsigned long offset)
+{
+	typeof(iter->shdr) *shdr = &iter->shdr;
+
+	mdi->nr_patch_target = shdr->sh_size / sizeof(long);
+	mdi->patch_target = xmalloc(shdr->sh_size);
+
+	elf_get_secdata(elf, iter);
+	elf_read_secdata(elf, iter, 0, mdi->patch_target, shdr->sh_size);
+
+	/* symbol has relative address, fix it to match each other */
+	if (elf->ehdr.e_type == ET_EXEC) {
+		unsigned long *patchable_loc = mdi->patch_target;
+		unsigned i;
+
+		for (i = 0; i < mdi->nr_patch_target; i++) {
+			patchable_loc[i] -= offset;
+		}
+	}
+}
+
 void mcount_arch_find_module(struct mcount_dynamic_info *mdi,
 			     struct symtab *symtab)
 {
@@ -158,6 +182,12 @@ void mcount_arch_find_module(struct mcount_dynamic_info *mdi,
 
 	elf_for_each_shdr(&elf, &iter) {
 		char *shstr = elf_get_name(&elf, &iter, iter.shdr.sh_name);
+
+		if (!strcmp(shstr, PATCHABLE_SECT)) {
+			mdi->type = DYNAMIC_PATCHABLE;
+			read_patchable_loc(mdi, &elf, &iter, mdi->base_addr);
+			goto out;
+		}
 
 		if (!strcmp(shstr, XRAY_SECT)) {
 			mdi->type = DYNAMIC_XRAY;
@@ -224,6 +254,36 @@ static int patch_fentry_func(struct mcount_dynamic_info *mdi, struct sym *sym)
 	/* only support calls to __fentry__ at the beginning */
 	if (memcmp(insn, nop1, sizeof(nop1)) &&  /* old pattern */
 	    memcmp(insn, nop2, sizeof(nop2))) {  /* new pattern */
+		pr_dbg("skip non-applicable functions: %s\n", sym->name);
+		return INSTRUMENT_FAILED;
+	}
+
+	/* get the jump offset to the trampoline */
+	target_addr = get_target_addr(mdi, (unsigned long)insn);
+	if (target_addr == 0)
+		return INSTRUMENT_SKIPPED;
+
+	/* make a "call" insn with 4-byte offset */
+	insn[0] = 0xe8;
+	/* hopefully we're not patching 'memcpy' itself */
+	memcpy(&insn[1], &target_addr, sizeof(target_addr));
+
+	pr_dbg3("update %p for '%s' function dynamically to call __fentry__\n",
+		insn, sym->name);
+
+	return INSTRUMENT_SUCCESS;
+}
+
+static int patch_patchable_func(struct mcount_dynamic_info *mdi, struct sym *sym)
+{
+	unsigned char patchable_gcc_nop[] = { 0x90, 0x90, 0x90, 0x90, 0x90 };
+	unsigned char patchable_clang_nop[] = { 0x0f, 0x1f, 0x44, 0x00, 0x08 };
+	unsigned char *insn = (void *)sym->addr + mdi->map->start;
+	unsigned int target_addr;
+
+	/* only support calls to __fentry__ at the beginning */
+	if (memcmp(insn, patchable_gcc_nop, sizeof(patchable_gcc_nop)) &&
+	    memcmp(insn, patchable_clang_nop, sizeof(patchable_clang_nop))) {
 		pr_dbg("skip non-applicable functions: %s\n", sym->name);
 		return INSTRUMENT_FAILED;
 	}
@@ -439,7 +499,7 @@ static int patch_normal_func(struct mcount_dynamic_info *mdi, struct sym *sym,
 		return state;
 	}
 
-	pr_dbg2("patch normal func: %s (patch size: %d)\n",
+	pr_dbg2("force patch normal func: %s (patch size: %d)\n",
 		sym->name, info.orig_size);
 
 	/*
@@ -551,6 +611,10 @@ int mcount_patch_func(struct mcount_dynamic_info *mdi, struct sym *sym,
 
 	case DYNAMIC_FENTRY_NOP:
 		result = patch_fentry_func(mdi, sym);
+		break;
+
+	case DYNAMIC_PATCHABLE:
+		result = patch_patchable_func(mdi, sym);
 		break;
 
 	case DYNAMIC_NONE:
