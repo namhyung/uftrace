131251f815bb2b3615c0be2e61a2d36d8651d645
Namhyung Kim <namhyung@gmail.com>
2022-03-02 23:39:15 -0800

    uftrace: Factor out event handling into utils/event.c
    There are places to handle event name and data.  Consolidate the
    processing into a single place so that it can be used by others
    easily.
    
    Rename those functions to have event_ prefix and add unit tests.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ utils/event.h
@@ -0,0 +1,38 @@
+#ifndef UFTRACE_EVENT_H
+#define UFTRACE_EVENT_H
+
+#include <stdbool.h>
+
+struct uftrace_data;
+
+/* please see man proc(5) for /proc/[pid]/statm */
+struct uftrace_proc_statm {
+	uint64_t		vmsize;  /* total program size in KB */
+	uint64_t		vmrss;   /* resident set size in KB */
+	uint64_t		shared;  /* shared rss in KB (Rssfile + RssShmem) */
+};
+
+struct uftrace_page_fault {
+	uint64_t		major;   /* major page faults */
+	uint64_t		minor;   /* minor page faults */
+};
+
+struct uftrace_pmu_cycle {
+	uint64_t		cycles;  /* cpu cycles */
+	uint64_t		instrs;  /* cpu instructions */
+};
+
+struct uftrace_pmu_cache {
+	uint64_t		refers;  /* cache references */
+	uint64_t		misses;  /* cache misses */
+};
+
+struct uftrace_pmu_branch {
+	uint64_t		branch;  /* branch instructions */
+	uint64_t		misses;  /* branch misses */
+};
+
+char * event_get_name(struct uftrace_data *handle, unsigned evt_id);
+char * event_get_data_str(unsigned evt_id, void *data, bool verbose);
+
+#endif  /* UFTRACE_EVENT_H */
--- utils/filter.h
+++ utils/filter.h
@@ -96,33 +96,6 @@ struct uftrace_filter_setting {
 	void				*info_str;
 };
 
-/* please see man proc(5) for /proc/[pid]/statm */
-struct uftrace_proc_statm {
-	uint64_t		vmsize;  /* total program size in KB */
-	uint64_t		vmrss;   /* resident set size in KB */
-	uint64_t		shared;  /* shared rss in KB (Rssfile + RssShmem) */
-};
-
-struct uftrace_page_fault {
-	uint64_t		major;
-	uint64_t		minor;
-};
-
-struct uftrace_pmu_cycle {
-	uint64_t		cycles;  /* cpu cycles */
-	uint64_t		instrs;  /* cpu instructions */
-};
-
-struct uftrace_pmu_cache {
-	uint64_t		refers;  /* cache references */
-	uint64_t		misses;  /* cache misses */
-};
-
-struct uftrace_pmu_branch {
-	uint64_t		branch;  /* branch instructions */
-	uint64_t		misses;  /* branch misses */
-};
-
 typedef void (*trigger_fn_t)(struct uftrace_trigger *tr, void *arg);
 
 struct symtabs;
