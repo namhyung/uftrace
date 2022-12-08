namhyung/uftrace

commit 52c7bd3cc320d52e21b44caa3bbf0b07a13656ed

live: Properly handle trigger options
Like filter options, it needs to ignore most of the trigger options
during replay since it's already handled in record.  But some actions
need to be handled in replay still - like 'color' action.

Signed-off-by: Namhyung Kim <namhyung@gmail.com>

cmds/live.c
@@ -37,6 +37,67 @@ static void reset_live_opts(struct uftrace_opts *opts)
 	free(opts->caller);
 	opts->caller = NULL;
 
+	/*
+	 * Likewise, most trigger options are ignored, but color settings
+	 * are effective only in replay.
+	 */
+	if (opts->trigger) {
+		char *new_triggers = NULL;
+		struct strv trs = STRV_INIT;
+		char *s;
+		int i;
+
+		if (strstr(opts->trigger, "color=") == NULL) {
+			free(opts->trigger);
+			opts->trigger = NULL;
+			goto others;
+		}
+
+		/*
+		 * Split trigger options for each function.
+		 * Example trigger option string like this:
+		 *
+		 *  a@depth=1;b@trace,read=pmu-cycle;c@color=red,time=1us
+		 */
+		strv_split(&trs, opts->trigger, ";");
+
+		strv_for_each(&trs, s, i) {
+			struct strv sv = STRV_INIT;
+			char *name, *tmp, *o;
+			int k;
+
+			if (strstr(s, "color=") == NULL)
+				continue;
+
+			name = xstrdup(s);
+			tmp = strchr(name, '@');
+			if (tmp == NULL) {
+				pr_dbg("invalid trigger option: %s\n", s);
+				continue;
+			}
+			*tmp = '\0';
+
+			/* split trigger option into actions */
+			strv_split(&sv, tmp + 1, ",");
+
+			strv_for_each(&sv, o, k) {
+				if (strncmp(o, "color=", 6))
+					continue;
+
+				tmp = strjoin(name, o, "@");
+				new_triggers = strjoin(new_triggers, tmp, ";");
+				free(tmp);
+				break;
+			}
+			strv_free(&sv);
+		}
+		strv_free(&trs);
+
+		free(opts->trigger);
+		opts->trigger = new_triggers;
+	}
+
+others:
 	opts->depth = MCOUNT_DEFAULT_DEPTH;
 	opts->disabled = false;
 	opts->no_event = false;
@@ -210,3 +271,40 @@ int command_live(int argc, char *argv[], struct uftrace_opts *opts)
 
 	return ret;
 }
+
+#ifdef UNIT_TEST
+
+TEST_CASE(live_reset_options)
+{
+	struct uftrace_opts o = {
+		.depth = 3,
+		.disabled = true,
+		.no_event = true,
+		.no_sched = true,
+	};
+
+	pr_dbg("setup live options\n");
+	o.filter = strjoin(o.filter, "foo", ";");
+	o.caller = strjoin(o.caller, "bar", ";");
+	/* add different types of triggers */
+	o.trigger = strjoin(o.trigger, "foo@filter,depth=1", ";");
+	o.trigger = strjoin(o.trigger, "bar@time=1us,color=red", ";");
+	o.trigger = strjoin(o.trigger, "baz@trace", ";");
+
+	pr_dbg("reset live options (filter, triggers, ...)\n");
+	reset_live_opts(&o);
+
+	TEST_EQ(o.depth, MCOUNT_DEFAULT_DEPTH);
+	TEST_EQ(o.filter, NULL);
+	TEST_EQ(o.caller, NULL);
+	/* it should only have the color trigger */
+	TEST_STREQ(o.trigger, "bar@color=red");
+	TEST_EQ(o.disabled, false);
+	TEST_EQ(o.no_event, false);
+	TEST_EQ(o.no_sched, false);
+
+	free(o.trigger);
+	return TEST_OK;
+}
+
+#endif /* UNIT_TEST */

commit 2a3025796570d45a5940171e87bf442126aae271

live: Keep 'backtrace' trigger action
Honggyu reported that the commit 52c7bd3cc320 ("live: Properly handle
trigger options") broke test 029 due to missing backtrace.

Indeed, it needs to keep the backtrace in reset_live_opts().  While at
it, add more comments and update the unit test.

Fixed: #1601

Signed-off-by: Namhyung Kim <namhyung@gmail.com>

cmds/live.c
@@ -38,8 +38,8 @@ static void reset_live_opts(struct uftrace_opts *opts)
 	opts->caller = NULL;
 
 	/*
-	 * Likewise, most trigger options are ignored, but color settings
-	 * are effective only in replay.
+	 * Likewise, most trigger options are ignored, but color settings and
+	 * backtrace are effective only in replay.
 	 */
 	if (opts->trigger) {
 		char *new_triggers = NULL;
@@ -47,7 +47,9 @@ static void reset_live_opts(struct uftrace_opts *opts)
 		char *s;
 		int i;
 
-		if (strstr(opts->trigger, "color=") == NULL) {
+		/* fastpath: these are not used frequently */
+		if (strstr(opts->trigger, "color=") == NULL &&
+		    strstr(opts->trigger, "backtrace") == NULL) {
 			free(opts->trigger);
 			opts->trigger = NULL;
 			goto others;
@@ -57,22 +59,25 @@ static void reset_live_opts(struct uftrace_opts *opts)
 		 * Split trigger options for each function.
 		 * Example trigger option string like this:
 		 *
-		 *  a@depth=1;b@trace,read=pmu-cycle;c@color=red,time=1us
+		 *  a@depth=1;b@backtrace,read=pmu-cycle;c@color=red,time=1us
 		 */
 		strv_split(&trs, opts->trigger, ";");
 
 		strv_for_each(&trs, s, i) {
 			struct strv sv = STRV_INIT;
 			char *name, *tmp, *o;
+			bool found = false;
 			int k;
 
-			if (strstr(s, "color=") == NULL)
+			/* skip this function if it doesn't have these triggers */
+			if (strstr(s, "color=") == NULL && strstr(s, "backtrace") == NULL)
 				continue;
 
 			name = xstrdup(s);
 			tmp = strchr(name, '@');
 			if (tmp == NULL) {
 				pr_dbg("invalid trigger option: %s\n", s);
+				free(name);
 				continue;
 			}
 			*tmp = '\0';
@@ -81,15 +86,25 @@ static void reset_live_opts(struct uftrace_opts *opts)
 			strv_split(&sv, tmp + 1, ",");
 
 			strv_for_each(&sv, o, k) {
-				if (strncmp(o, "color=", 6))
+				if (strncmp(o, "color=", 6) && strcmp(o, "backtrace"))
 					continue;
 
-				tmp = strjoin(name, o, "@");
-				new_triggers = strjoin(new_triggers, tmp, ";");
-				free(tmp);
-				break;
+				if (!found) {
+					/* first action: needs func name first */
+					tmp = strjoin(name, o, "@");
+					found = true;
+					continue;
+				}
+
+				/* second or later actions: just append */
+				tmp = strjoin(tmp, o, ",");
 			}
 			strv_free(&sv);
+
+			if (found) {
+				new_triggers = strjoin(new_triggers, tmp, ";");
+				free(tmp);
+			}
 		}
 		strv_free(&trs);
 
@@ -289,7 +304,7 @@ TEST_CASE(live_reset_options)
 	/* add different types of triggers */
 	o.trigger = strjoin(o.trigger, "foo@filter,depth=1", ";");
 	o.trigger = strjoin(o.trigger, "bar@time=1us,color=red", ";");
-	o.trigger = strjoin(o.trigger, "baz@trace", ";");
+	o.trigger = strjoin(o.trigger, "baz@backtrace,trace,color=blue", ";");
 
 	pr_dbg("reset live options (filter, triggers, ...)\n");
 	reset_live_opts(&o);
@@ -298,7 +313,7 @@ TEST_CASE(live_reset_options)
 	TEST_EQ(o.filter, NULL);
 	TEST_EQ(o.caller, NULL);
 	/* it should only have the color trigger */
-	TEST_STREQ(o.trigger, "bar@color=red");
+	TEST_STREQ(o.trigger, "bar@color=red;baz@backtrace,color=blue");
 	TEST_EQ(o.disabled, false);
 	TEST_EQ(o.no_event, false);
 	TEST_EQ(o.no_sched, false);