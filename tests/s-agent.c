e3c984bb48b6ed97ba3d0774c4cd32a32a407d28
Clément Guidi <cguidi@ciena.com>
2023-02-21 11:51:20 -0500

    tests: Add t287 for runtime arguments and retval
    Test collecting new arguments and return values at runtime.
    
    Signed-off-by: Clément Guidi <cguidi@ciena.com>

--- tests/s-agent.c
+++ tests/s-agent.c
@@ -10,12 +10,13 @@
 #include <string.h>
 #include <unistd.h>
 
-static int func(int depth);
-static int trigger(int depth);
+static int func(int depth, char *s);
+static int trigger(int depth, char *s);
 static int a(int depth);
 static int b(int depth);
 static int c(int depth);
 
+#define CAFE 0xCAFE
 #define DELAY 1000
 static int use_delay;
 
@@ -23,22 +24,24 @@ static int use_delay;
    the compiler */
 static int dummy;
 
-static int func(int depth)
+static int func(int depth, char *s)
 {
 	dummy++;
+	(*(__volatile__ __typeof__(s) *)&(s)); /* prevent compiler optimization for s */
 	if (depth)
 		return a(--depth) + 1;
 
-	return 0;
+	return CAFE;
 }
 
-static int trigger(int depth)
+static int trigger(int depth, char *s)
 {
 	dummy--;
+	(*(__volatile__ __typeof__(s) *)&(s)); /* prevent compiler optimization for s */
 	if (depth)
 		return a(--depth) + 1;
 
-	return 0;
+	return CAFE;
 }
 
 static int a(int depth)
@@ -48,7 +51,7 @@ static int a(int depth)
 	if (depth)
 		return b(--depth) + 1;
 
-	return 0;
+	return CAFE;
 }
 
 static int b(int depth)
@@ -58,7 +61,7 @@ static int b(int depth)
 	if (depth)
 		return c(--depth) + 1;
 
-	return 0;
+	return CAFE;
 }
 
 static int c(int depth)
@@ -66,7 +69,7 @@ static int c(int depth)
 	if (use_delay)
 		usleep(DELAY);
 	if (getpid()) /* true */
-		return depth;
+		return CAFE - depth;
 
 	return -1; /* unreached */
 }
@@ -76,23 +79,26 @@ int main(int argc, char *argv[])
 	int i;
 	int depth = 3;
 	int use_trigger = 0;
+	char *s = NULL;
 
 	for (i = 1; i < argc; i++) {
 		if (!strcmp(argv[i], "--depth"))
 			depth = atoi(argv[++i]);
-		if (!strcmp(argv[i], "--trigger"))
+		else if (!strcmp(argv[i], "--trigger"))
 			use_trigger = 1;
-		if (!strcmp(argv[i], "--delay"))
+		else if (!strcmp(argv[i], "--delay"))
 			use_delay = 1;
+		else
+			s = argv[i];
 	}
 
 	if (depth <= 0)
 		depth = 1;
 
 	do {
-		func(depth);
+		func(depth, s);
 		if (use_trigger)
-			trigger(depth);
+			trigger(depth, s);
 	} while (getchar() != EOF);
 
 	return 0;
