06d2cdf945e372b8dc868c3e5823af4bf94f3db4
Clément Guidi <cguidi@ciena.com>
2023-04-10 08:52:33 -0400

    dynamic: Batch function unpatching process
    Synchronization requires sending signals to threads inside the process.
    This is performed for every symbols, which means a lot of signal can be
    sent. This has a major performance impact. This commit batches the
    initial and final steps of the patching process so we only need to send
    one signal per thread for every batch.
    
    Co-authored-by: Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
    Signed-off-by: Clément Guidi <cguidi@ciena.com>

--- libmcount/dynamic.c
+++ libmcount/dynamic.c
@@ -229,6 +229,10 @@ __weak void mcount_patch_normal_func_fini(void)
 {
 }
 
+__weak void mcount_unpatch_normal_func_fini(void)
+{
+}
+
 __weak int mcount_unpatch_func(struct mcount_dynamic_info *mdi, struct uftrace_symbol *sym)
 {
 	return -1;
@@ -572,6 +576,7 @@ static void patch_normal_func_matched(struct mcount_dynamic_info *mdi, struct uf
 		stats.nomatch++;
 
 	mcount_patch_normal_func_fini();
+	mcount_unpatch_normal_func_fini();
 
 	free(soname);
 }
