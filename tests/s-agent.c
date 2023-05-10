a6fb766fdb1e1d6e767781e22f7e105419e3787c
Clément Guidi <cguidi@ciena.com>
2023-05-10 10:49:32 -0400

    tests: Refactor 's-agent.c'
    Add a loop in 's-agent.c' so we can test multiple agent commands.
    
    Signed-off-by: Clément Guidi <cguidi@ciena.com>

--- tests/s-agent.c
+++ tests/s-agent.c
@@ -1,11 +1,65 @@
-/*
- *  This is a test to see if the agent is running and responsive.
+/* This target program is intended for use with the agent. It executes a loop
+ * and awaits for external input at each iteration.
+ *
+ * It accepts options so it can adapt its execution to test various agent
+ * features.
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static int func(int depth);
+static int a(int depth);
+static int b(int depth);
+static int c(int depth);
+
+static int func(int depth)
+{
+	if (depth)
+		return a(--depth) + 1;
+
+	return 0;
+}
+
+static int a(int depth)
+{
+	if (depth)
+		return b(--depth) + 1;
+
+	return 0;
+}
+
+static int b(int depth)
+{
+	if (depth)
+		return c(--depth) + 1;
+
+	return 0;
+}
+
+static int c(int depth)
+{
+	if (getpid()) /* true */
+		return depth;
+
+	return -1; /* unreached */
+}
 
 int main(int argc, char *argv[])
 {
-	getchar();
+	int i;
+	int depth = 3;
+
+	for (i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "--depth"))
+			depth = atoi(argv[++i]);
+	}
+
+	do {
+		func(depth);
+	} while (getchar() != EOF);
+
 	return 0;
 }
--- tests/t273_agent_basic.py
+++ tests/t273_agent_basic.py
@@ -9,35 +9,36 @@ class TestCase(TestBase):
     def __init__(self):
         TestBase.__init__(self, 'agent', """
 # DURATION     TID     FUNCTION
-            [ 22621] | main() {
-  43.742 ms [ 22621] |   getchar();
-  43.759 ms [ 22621] | } /* main */
+  34.330 ms [ 22621] | main();
 """)
 
-    def prerun(self, timeout):
-        self.subcmd = 'record'
-        self.option = '--keep-pid -g'
-        self.exearg = 't-' + self.name
-        record_cmd  = self.runcmd()
-        self.pr_debug("prerun command: " + record_cmd)
-        record_p = sp.Popen(record_cmd.split(), stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
-
-        sleep(.05)              # time for the agent to start
+    def client_send_command(self, pid, option):
         self.subcmd = 'live'
-        self.option = '-p %d' % record_p.pid
+        self.option = '-p %d %s' % (pid, option)
         self.exearg = ''
         client_cmd = self.runcmd()
         self.pr_debug('prerun command: ' + client_cmd)
-        client_ret = sp.call(client_cmd.split())
+        client_p = sp.run(client_cmd.split())
+        return client_p.returncode
 
-        record_p.communicate(b"^D") # target waits for a char to end
-        record_p.wait()
+    def prerun(self, timeout):
+        self.subcmd = 'record'
+        self.option  = '--agent'
+        self.option += ' --keep-pid'
+        self.option += ' --no-libcall'
+        self.exearg = 't-' + self.name
+        record_cmd  = self.runcmd()
+        self.pr_debug("prerun command: " + record_cmd)
+        record_p = sp.Popen(record_cmd.split(), stdin=sp.PIPE, stderr=sp.PIPE, bufsize=0)
 
-        if client_ret != 0:
+        sleep(.05)              # time for the agent to start
+        if self.client_send_command(record_p.pid, '') != 0:
             return TestBase.TEST_NONZERO_RETURN
+        record_p.stdin.close()
+
         return TestBase.TEST_SUCCESS
 
     def setup(self):
         self.subcmd = 'replay'
-        self.option = ''
+        self.option = '-D 1'
         self.exearg = ''
