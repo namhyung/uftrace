0f6931a5b20f9b6c01e3b673c597f90f7c344c7d
Yi Hong <zouzou0208@gmail.com>
2023-05-14 20:58:24 +0800

    build: Fix a compiler error on x86_64 build   $ make     CC FPIC  arch/x86_64/mcount-insn.o     uftrace/arch/x86_64/mcount-insn.c:479:3: error: label at \     end of compound statement     default:     ^~~~~~~
    Fixed: #1707
    Signed-off-by: Yi Hong <zouzou0208@gmail.com>

--- arch/x86_64/mcount-insn.c
+++ arch/x86_64/mcount-insn.c
@@ -478,6 +478,7 @@ static int check_instrumentable(struct mcount_disasm_engine *disasm, cs_insn *in
 			return INSTRUMENT_FAIL_NO_DETAIL;
 		default:
 			/* do nothing for legit instructions. */
+			break;
 		}
 	}
 
