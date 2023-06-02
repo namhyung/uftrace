4d8afdd13e8b64061f267941c7d1bc72ecdfe429
Namhyung Kim <namhyung@gmail.com>
2023-06-01 22:37:43 -0700

    test: Update python libcall tests
    As it treats modules under the same directory as the main script,
    it should check external library calls.
    
    I randomly chose json module for the test and calls json.dumps()
    to see its internal functions.  But there's a risk that the
    implementation can be different on each version.  In my setup,
    it calls json.encoder.JSONEncoder.encode() function.  Hope it
    does not change often.
    
      $ uftrace --nest-libcall -N ^importlib -D6  s-libmain.py
      # DURATION     TID     FUNCTION
                  [ 12889] | __main__.<module>() {
                  [ 12889] |   myfunc() {
                  [ 12889] |     mymod.public_func() {
                  [ 12889] |       mymod.internal() {
                  [ 12889] |         json.dumps() {
        16.521 us [ 12889] |           json.encoder.JSONEncoder.encode();
        19.719 us [ 12889] |         } /* json.dumps */
        23.313 us [ 12889] |       } /* mymod.internal */
                  [ 12889] |       mymod.internal() {
                  [ 12889] |         json.dumps() {
         6.233 us [ 12889] |           json.encoder.JSONEncoder.encode();
         7.311 us [ 12889] |         } /* json.dumps */
         8.484 us [ 12889] |       } /* mymod.internal */
        34.855 us [ 12889] |     } /* mymod.public_func */
        37.884 us [ 12889] |   } /* myfunc */
        10.610 ms [ 12889] | } /* __main__.<module> */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- tests/p010_libcall_none.py
+++ tests/p010_libcall_none.py
@@ -7,8 +7,14 @@ class TestCase(PyTestBase):
         PyTestBase.__init__(self, 'libmain', """
 # DURATION     TID     FUNCTION
             [ 12778] | __main__.<module>() {
-   6.257 us [ 12778] |   myfunc();
- 593.772 us [ 12778] | } /* __main__.<module> */
+   8.153 ms [ 12778] |   mymod.<module>();
+            [ 12778] |   myfunc() {
+            [ 12778] |     mymod.public_func() {
+  22.748 us [ 12778] |       mymod.internal();
+   6.878 us [ 12778] |       mymod.internal();
+  32.405 us [ 12778] |     } /* mymod.public_func */
+  35.086 us [ 12778] |   } /* myfunc */
+   8.787 ms [ 12778] | } /* __main__.<module> */
 """)
 
     def setup(self):
