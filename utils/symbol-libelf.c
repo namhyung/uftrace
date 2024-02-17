b88ceceb403314868a321fa57fc60c15203d01bf
Namhyung Kim <namhyung@gmail.com>
2024-02-17 12:46:54 -0800

    symbol: Add elf_retry() to use debug info if possible
    Modern distros provide separate debug packages for system binaries.
    As it added the separate debug file support, it can also use it to find
    symbol table of the binary too.  And with the debug info, it can support
    argument and return values.
    
    The below is an example to use dynamic tracing for the nproc binary and
    to show automatic argument/return values.
    
      $ uftrace -a -P. --no-libcall nproc
      8
      # DURATION     TID     FUNCTION
                  [2450185] | main(1, 0x7ffce57b5ec8) {
       143.049 us [2450185] |   set_program_name("nproc");
         0.769 us [2450185] |   atexit();
                  [2450185] |   num_processors(NPROC_CURRENT_OVERRIDABLE) {
         0.194 us [2450185] |     parse_omp_threads("NULL") = 0;
         0.059 us [2450185] |     parse_omp_threads("NULL") = 0;
         4.007 us [2450185] |   } = 8; /* num_processors */
       330.818 us [2450185] | } = 0; /* main */
                  [2450185] | close_stdout() {
                  [2450185] |   close_stream(&_IO_2_1_stdout_) {
                  [2450185] |     rpl_fclose(&_IO_2_1_stdout_) {
         0.507 us [2450185] |       rpl_fflush(&_IO_2_1_stdout_) = 0;
         2.621 us [2450185] |     } = 0; /* rpl_fclose */
         3.535 us [2450185] |   } = 0; /* close_stream */
                  [2450185] |   close_stream(&_IO_2_1_stderr_) {
                  [2450185] |     rpl_fclose(&_IO_2_1_stderr_) {
         0.113 us [2450185] |       rpl_fflush(&_IO_2_1_stderr_) = 0;
         0.704 us [2450185] |     } = 0; /* rpl_fclose */
         0.911 us [2450185] |   } = 0; /* close_stream */
         4.907 us [2450185] | } /* close_stdout */
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/symbol-libelf.c
+++ utils/symbol-libelf.c
@@ -8,11 +8,15 @@
 #define PR_FMT "symbol"
 #define PR_DOMAIN DBG_SYMBOL
 
+#include "utils/dwarf.h"
 #include "utils/symbol-libelf.h"
 #include "utils/utils.h"
 
 int elf_init(const char *filename, struct uftrace_elf_data *elf)
 {
+	/* it will be set only in elf_retry() */
+	elf->dwfl = NULL;
+
 	elf->fd = open(filename, O_RDONLY);
 	if (elf->fd < 0) {
 		pr_dbg("error during open ELF file: %s: %m\n", filename);
@@ -49,11 +53,67 @@ void elf_finish(struct uftrace_elf_data *elf)
 	if (elf->fd < 0)
 		return;
 
+#ifdef HAVE_LIBDW
+	if (elf->dwfl) {
+		/* it'll close ELF handle and FD */
+		dwfl_end(elf->dwfl);
+		return;
+	}
+#endif
+
 	elf_end(elf->handle);
 	elf->handle = NULL;
 
 	close(elf->fd);
 	elf->fd = -1;
+}
+
+/* return 1 if it wants to retry with libdwfl, 0 otherwise */
+int elf_retry(const char *filename, struct uftrace_elf_data *elf)
+{
+#ifdef HAVE_LIBDW
+	Dwfl *dwfl;
+	Dwfl_Module *mod;
+	Dwarf_Addr bias;
+	Dwarf *dw;
+
+	/* it already tried (and failed), no retry */
+	if (elf->dwfl)
+		return 0;
+
+	dwfl = dwfl_begin(&dwfl_callbacks);
+	if (dwfl == NULL) {
+		pr_dbg("dwfl_begin() failed\n");
+		return 0;
+	}
+
+	mod = dwfl_report_offline(dwfl, filename, filename, elf->fd);
+	if (mod == NULL) {
+		pr_dbg("cannot report file: %s\n", dwfl_errmsg(dwfl_errno()));
+		goto out;
+	}
+
+	dw = dwfl_module_getdwarf(mod, &bias);
+	if (dw == NULL) {
+		pr_dbg("cannot find debug file: %s\n", dwfl_errmsg(dwfl_errno()));
+		goto out;
+	}
+
+	/* invalidate the existing ELF */
+	elf_end(elf->handle);
+
+	/* update the ELF handle from the (separate) debug file */
+	elf->handle = dwarf_getelf(dw);
+
+	if (elf->handle) {
+		elf->dwfl = dwfl;
+		return 1;
+	}
+
+out:
+	dwfl_end(dwfl);
+#endif
+	return 0;
 }
 
 void elf_get_secdata(struct uftrace_elf_data *elf, struct uftrace_elf_iter *iter)
