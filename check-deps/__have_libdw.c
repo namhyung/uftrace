054167e62c34d647a5dbe37dba6fceb3d5bd17b4
Namhyung Kim <namhyung@gmail.com>
2024-02-18 14:10:58 -0800

    build: Check libdwfl as well as libdw
    The libdwfl is a frontend library and distributed in libdw (it's in a
    same file).  So rather than creating a new check, let's do it together.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- check-deps/__have_libdw.c
+++ check-deps/__have_libdw.c
@@ -1,10 +1,14 @@
 #include <elfutils/libdw.h>
+#include <elfutils/libdwfl.h>
 
 int main(void)
 {
 	Dwarf *dw;
 	Dwarf_Die die;
+	Dwfl *dwfl;
+	Dwfl_Callbacks dwfl_callbacks;
 
+	/* libdw API test */
 	dw = dwarf_begin(0, DWARF_C_READ);
 
 	if (dwarf_offdie(dw, 0, &die)) {
@@ -14,5 +18,11 @@ int main(void)
 	}
 
 	dwarf_end(dw);
+
+	/* libdwfl API test (in elfutils/libdw 0.122) */
+	dwfl = dwfl_begin(&dwfl_callbacks);
+	dwfl_report_offline(dwfl, "", "", -1);
+	dwfl_end(dwfl);
+
 	return 0;
 }
