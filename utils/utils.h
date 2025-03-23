b8cfd286f1cc7812c3786a8260ee16edcbbb38d7
Namhyung Kim <namhyung@gmail.com>
2025-03-23 15:01:02 -0700

    report: Add missing stdv fields for diff
    The new total-stdv and self-stdv fields were added but they are missing
    in the diff tables.  So using one of them in the diff mode get a crash.
    This can be easily tested like:
    
      $ uftrace report --avg-total --diff=uftrace.data.old
      Segmentation fault (core dumped)
    
    Fixed: #1970
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/utils.h
+++ utils/utils.h
@@ -366,6 +366,7 @@ void print_time_unit(uint64_t delta_nsec);
 void print_diff_percent(uint64_t base_nsec, uint64_t delta_nsec);
 void print_diff_time_unit(uint64_t base_nsec, uint64_t pair_nsec);
 void print_diff_count(uint64_t base, uint64_t pair);
+void print_diff_percent_point(double base, double pair);
 
 char *setup_pager(void);
 void start_pager(char *pager);
